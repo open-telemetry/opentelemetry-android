ffeb228c74842b24cd3cc4ce8a43ad55eb820b3b
John Watson <jkwatson@gmail.com>
2021-10-11 14:01:20 -0700

    Add browser rum to the local WebView content (#172)
    * Add browser rum to the local WebView content
    
    * get the realm from the java as well

--- sample-app/src/main/java/com/splunk/android/sample/WebViewFragment.java
+++ sample-app/src/main/java/com/splunk/android/sample/WebViewFragment.java
@@ -100,5 +100,15 @@ public class WebViewFragment extends Fragment {
             SplunkRum.getInstance().addRumEvent("WebViewButtonClicked", Attributes.empty());
             Toast.makeText(context, toast, Toast.LENGTH_LONG).show();
         }
+
+        @JavascriptInterface
+        public String getRumAccessToken() {
+            return context.getResources().getString(R.string.rum_access_token);
+        }
+
+        @JavascriptInterface
+        public String getRumRealm() {
+            return context.getResources().getString(R.string.rum_realm);
+        }
     }
 }
