0b00b15b8926cbb4bfb833cfc23f5953827785ca
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-01-27 17:00:24 +0100

    Use DelegatingSpanData (from OTel SDK) (#220)

--- splunk-otel-android/src/main/java/com/splunk/rum/ModifiedSpanData.java
+++ splunk-otel-android/src/main/java/com/splunk/rum/ModifiedSpanData.java
@@ -16,55 +16,16 @@
 
 package com.splunk.rum;
 
-import java.util.List;
-
 import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.api.trace.SpanContext;
-import io.opentelemetry.api.trace.SpanKind;
-import io.opentelemetry.sdk.common.InstrumentationLibraryInfo;
-import io.opentelemetry.sdk.resources.Resource;
-import io.opentelemetry.sdk.trace.data.EventData;
-import io.opentelemetry.sdk.trace.data.LinkData;
+import io.opentelemetry.sdk.trace.data.DelegatingSpanData;
 import io.opentelemetry.sdk.trace.data.SpanData;
-import io.opentelemetry.sdk.trace.data.StatusData;
 
-final class ModifiedSpanData implements SpanData {
-    private final SpanData original;
+final class ModifiedSpanData extends DelegatingSpanData {
     private final Attributes modifiedAttributes;
 
     ModifiedSpanData(SpanData original, Attributes modifiedAttributes) {
-        this.original = original;
+        super(original);
         this.modifiedAttributes = modifiedAttributes;
-    }
-
-    @Override
-    public String getName() {
-        return original.getName();
-    }
-
-    @Override
-    public SpanKind getKind() {
-        return original.getKind();
-    }
-
-    @Override
-    public SpanContext getSpanContext() {
-        return original.getSpanContext();
-    }
-
-    @Override
-    public SpanContext getParentSpanContext() {
-        return original.getParentSpanContext();
-    }
-
-    @Override
-    public StatusData getStatus() {
-        return original.getStatus();
-    }
-
-    @Override
-    public long getStartEpochNanos() {
-        return original.getStartEpochNanos();
     }
 
     @Override
@@ -73,58 +34,7 @@ final class ModifiedSpanData implements SpanData {
     }
 
     @Override
-    public List<EventData> getEvents() {
-        return original.getEvents();
-    }
-
-    @Override
-    public List<LinkData> getLinks() {
-        return original.getLinks();
-    }
-
-    @Override
-    public long getEndEpochNanos() {
-        return original.getEndEpochNanos();
-    }
-
-    @Override
-    public boolean hasEnded() {
-        return original.hasEnded();
-    }
-
-    @Override
-    public int getTotalRecordedEvents() {
-        return original.getTotalRecordedEvents();
-    }
-
-    @Override
-    public int getTotalRecordedLinks() {
-        return original.getTotalRecordedLinks();
-    }
-
-    @Override
     public int getTotalAttributeCount() {
-        // TODO: not sure about this
         return modifiedAttributes.size();
     }
-
-    @Override
-    public InstrumentationLibraryInfo getInstrumentationLibraryInfo() {
-        return original.getInstrumentationLibraryInfo();
-    }
-
-    @Override
-    public Resource getResource() {
-        return original.getResource();
-    }
-
-    @Override
-    public String toString() {
-        return "ModifiedSpanData{" +
-                "original=" + original +
-                ", modifiedAttributes=" + modifiedAttributes +
-                '}';
-    }
-
-    // TODO: hashCode, equals ?
 }
