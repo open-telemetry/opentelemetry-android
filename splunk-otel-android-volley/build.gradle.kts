19c7796e28dab3c2fcc298222f620e3b7732c434
Lauri Tulmin <ltulmin@splunk.com>
2022-04-13 00:05:20 +0300

    Disable desugaring for volley (#271)

--- splunk-otel-android-volley/build.gradle.kts
+++ splunk-otel-android-volley/build.gradle.kts
@@ -24,8 +24,6 @@ android {
         }
     }
     compileOptions {
-        isCoreLibraryDesugaringEnabled = true
-
         sourceCompatibility(JavaVersion.VERSION_1_8)
         targetCompatibility(JavaVersion.VERSION_1_8)
     }
