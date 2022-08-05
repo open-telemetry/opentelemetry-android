84518918b0db9f9ffbddc9460e891dbaf51d6a20
Mateusz Rzeszutek <mrzeszutek@splunk.com>
2022-08-05 11:52:08 +0200

    Bump AGP and spotless versions (#326)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     `kotlin-dsl`
 
     // When updating, update below in dependencies too
-    id("com.diffplug.spotless") version "6.5.2"
+    id("com.diffplug.spotless") version "6.9.0"
 }
 
 spotless {
@@ -20,7 +20,7 @@ repositories {
 
 dependencies {
     // keep this version in sync with /build.gradle.kts
-    implementation("com.android.tools.build:gradle:7.2.1")
+    implementation("com.android.tools.build:gradle:7.2.2")
 
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.5.2")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.9.0")
 }
