084fa649b3cd284c04d3c3117bb960e3e25f5b52
Lauri Tulmin <ltulmin@splunk.com>
2023-06-16 16:49:27 +0300

    Remove deprecated targetSdk (#581)

--- splunk-otel-android/build.gradle.kts
+++ splunk-otel-android/build.gradle.kts
@@ -12,7 +12,6 @@ android {
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 33
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
