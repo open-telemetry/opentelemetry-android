ffa5cf120cd378d912d59f8f526efbde327c5cd1
Jason Plumb <jplumb@splunk.com>
2023-07-24 10:54:11 -0700

    remove publishing block until we are ready (and to probably make us publish more like the other java components)

--- buildSrc/src/main/kotlin/otel.android-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/otel.android-library-conventions.gradle.kts
@@ -16,65 +16,6 @@ android {
     }
 }
 
-publishing {
-    repositories {
-        maven {
-            val releasesRepoUrl = URI("https://oss.sonatype.org/service/local/staging/deploy/maven2")
-            val snapshotsRepoUrl = URI("https://oss.sonatype.org/content/repositories/snapshots/")
-            url = if (project.findProperty("release") == "true") releasesRepoUrl else snapshotsRepoUrl
-            credentials {
-                username = findProperty("mavenCentralUsername") as String?
-                password = findProperty("mavenCentralPassword") as String?
-            }
-        }
-    }
-    publications {
-        register<MavenPublication>("maven") {
-            groupId = "io.opentelemetry.android"
-            artifactId = base.archivesName.get()
-
-            afterEvaluate {
-                pom.name.set("${project.extra["pomName"]}")
-                pom.description.set(project.description)
-            }
-
-            pom {
-                url.set("https://github.com/open-telemetry/opentelemetry-android")
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("opentelemetry")
-                        name.set("OpenTelemetry Authors")
-                        email.set("support+java@opentelemetry.io")
-                        organization.set("OpenTelemetry")
-                        organizationUrl.set("https://opentelemetry.io")
-                    }
-                }
-                scm {
-                    connection.set("https://github.com/open-telemetry/opentelemetry-android.git")
-                    developerConnection.set("https://github.com/open-telemetry/opentelemetry-android.git")
-                    url.set("https://github.com/open-telemetry/opentelemetry-android")
-                }
-            }
-        }
-    }
-}
-
-if (project.findProperty("release") == "true") {
-    signing {
-        useGpgCmd()
-        val signingKey: String? by project
-        val signingPassword: String? by project
-        useInMemoryPgpKeys(signingKey, signingPassword)
-        sign(publishing.publications["maven"])
-    }
-}
-
 val sourcesJar by tasks.registering(Jar::class) {
 
     from(android.sourceSets.named("main").get().java.srcDirs)
