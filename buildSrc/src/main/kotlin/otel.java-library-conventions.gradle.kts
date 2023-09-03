c5cce3435c7613120f9723df43fde0b51873329f
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2023-09-03 13:01:32 +0200

    Adding animal sniffer to java libs

--- buildSrc/src/main/kotlin/otel.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/otel.java-library-conventions.gradle.kts
@@ -1,4 +1,25 @@
+import ru.vyarus.gradle.plugin.animalsniffer.AnimalSniffer
+
 plugins {
     id("java-library")
     id("otel.errorprone-conventions")
+    id("ru.vyarus.animalsniffer")
+}
+
+dependencies {
+    signature("com.toasttab.android:gummy-bears-api-${project.property("android.minSdk")}:0.5.1@signature")
+}
+
+animalsniffer {
+    sourceSets = listOf(java.sourceSets.main.get())
+}
+
+// Always having declared output makes this task properly participate in tasks up-to-date checks
+tasks.withType<AnimalSniffer> {
+    reports.text.required.set(true)
+}
+
+// Attaching animalsniffer check to the compilation process.
+tasks.named("classes").configure {
+    finalizedBy("animalsnifferMain")
 }
