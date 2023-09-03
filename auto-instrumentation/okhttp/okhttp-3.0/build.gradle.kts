0e894ebb753b27eb4cd564afac805ec8bdd7c18a
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2023-09-03 16:14:32 +0200

    Removing okhttp module

--- auto-instrumentation/okhttp/okhttp-3.0/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("otel.java-library-conventions")
-    id("otel.publish-conventions")
-}
-
-val otelVersion = project.property("otel.sdk.version")
-dependencies {
-    implementation("io.opentelemetry.instrumentation:opentelemetry-okhttp-3.0:$otelVersion-alpha")
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,3 @@
 rootProject.name = "opentelemetry-android"
 
 include(":instrumentation")
-include(":auto-instrumentation:okhttp:okhttp-3.0")
