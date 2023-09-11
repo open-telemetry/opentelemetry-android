9e4059610d24c7f461b9be0bf3f36fb166eddb5b
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2023-09-11 15:36:12 +0200

    Temporarily fixing OkHttp3Singletons

--- auto-instrumentation/okhttp/okhttp-3.0/library/src/main/java/io/opentelemetry/instrumentation/library/okhttp/v3_0/OkHttp3Singletons.java
+++ auto-instrumentation/okhttp/okhttp-3.0/library/src/main/java/io/opentelemetry/instrumentation/library/okhttp/v3_0/OkHttp3Singletons.java
@@ -5,41 +5,42 @@
 
 package io.opentelemetry.instrumentation.library.okhttp.v3_0;
 
-import static java.util.Collections.singletonList;
+import java.util.Collections;
 
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
 import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter;
 import io.opentelemetry.instrumentation.api.instrumenter.http.HttpClientResend;
-import io.opentelemetry.instrumentation.api.instrumenter.net.PeerServiceAttributesExtractor;
 import io.opentelemetry.instrumentation.okhttp.v3_0.internal.ConnectionErrorSpanInterceptor;
-import io.opentelemetry.instrumentation.okhttp.v3_0.internal.OkHttpAttributesGetter;
 import io.opentelemetry.instrumentation.okhttp.v3_0.internal.OkHttpInstrumenterFactory;
 import io.opentelemetry.instrumentation.okhttp.v3_0.internal.TracingInterceptor;
-import io.opentelemetry.javaagent.bootstrap.internal.CommonConfig;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
 
-/** Holder of singleton interceptors for adding to instrumented clients. */
+/**
+ * Holder of singleton interceptors for adding to instrumented clients.
+ */
 public final class OkHttp3Singletons {
 
     private static final Instrumenter<Request, Response> INSTRUMENTER =
-            OkHttpInstrumenterFactory.create(
-                    GlobalOpenTelemetry.get(),
-                    builder ->
-                            builder.setCapturedRequestHeaders(
-                                            CommonConfig.get().getClientRequestHeaders())
-                                    .setCapturedResponseHeaders(
-                                            CommonConfig.get().getClientResponseHeaders())
-                                    .setKnownMethods(
-                                            CommonConfig.get().getKnownHttpRequestMethods()),
-                    singletonList(
-                            PeerServiceAttributesExtractor.create(
-                                    OkHttpAttributesGetter.INSTANCE,
-                                    CommonConfig.get().getPeerServiceMapping())),
-                    CommonConfig.get().shouldEmitExperimentalHttpClientMetrics());
+            OkHttpInstrumenterFactory.create(GlobalOpenTelemetry.get(), requestResponseHttpClientAttributesExtractorBuilder -> {
+            }, Collections.emptyList(), false);
+//            OkHttpInstrumenterFactory.create(
+//                    GlobalOpenTelemetry.get(),
+//                    builder ->
+//                            builder.setCapturedRequestHeaders(
+//                                            CommonConfig.get().getClientRequestHeaders())
+//                                    .setCapturedResponseHeaders(
+//                                            CommonConfig.get().getClientResponseHeaders())
+//                                    .setKnownMethods(
+//                                            CommonConfig.get().getKnownHttpRequestMethods()),
+//                    singletonList(
+//                            PeerServiceAttributesExtractor.create(
+//                                    OkHttpAttributesGetter.INSTANCE,
+//                                    CommonConfig.get().getPeerServiceMapping())),
+//                    CommonConfig.get().shouldEmitExperimentalHttpClientMetrics());
 
     public static final Interceptor CONTEXT_INTERCEPTOR =
             chain -> {
@@ -54,5 +55,6 @@ public final class OkHttp3Singletons {
     public static final Interceptor TRACING_INTERCEPTOR =
             new TracingInterceptor(INSTRUMENTER, GlobalOpenTelemetry.getPropagators());
 
-    private OkHttp3Singletons() {}
+    private OkHttp3Singletons() {
+    }
 }
