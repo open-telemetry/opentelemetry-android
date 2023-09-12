71ffde6f8a2027a2ec3e3d1406bb64432bfb6dcc
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2023-09-12 15:32:54 +0200

    Configuring android tests with junit5

--- buildSrc/src/main/kotlin/otel.android-app-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/otel.android-app-conventions.gradle.kts
@@ -12,6 +12,11 @@ android {
 
     defaultConfig {
         minSdk = (project.property("android.minSdk") as String).toInt()
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunnerArguments.set(
+            "runnerBuilder",
+            "de.mannodermaus.junit5.AndroidJUnit5Builder"
+        )
     }
 
     compileOptions {
@@ -19,4 +24,17 @@ android {
         sourceCompatibility(javaVersion)
         targetCompatibility(javaVersion)
     }
+
+    packaging {
+        resources.excludes += "META-INF/LICENSE*.md"
+    }
+}
+
+val androidXTestVersion = "1.5.0"
+dependencies {
+    androidTestImplementation("androidx.test:runner:1.4.0")
+    androidTestImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+
+    androidTestImplementation("de.mannodermaus.junit5:android-test-core:1.2.2")
+    androidTestRuntimeOnly("de.mannodermaus.junit5:android-test-runner:1.2.2")
 }
