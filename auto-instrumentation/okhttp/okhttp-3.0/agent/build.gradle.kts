36d9a4d3d6164b3cf1c77e948c17824a27cd54bc
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2023-09-18 15:00:45 +0200

    Adding byte buddy version to properties

--- auto-instrumentation/okhttp/okhttp-3.0/agent/build.gradle.kts
+++ auto-instrumentation/okhttp/okhttp-3.0/agent/build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
 
 dependencies {
     implementation(project(":auto-instrumentation:okhttp:okhttp-3.0:library"))
-    implementation("net.bytebuddy:byte-buddy:1.12.22")
+    implementation("net.bytebuddy:byte-buddy:${property("bytebuddy.version")}")
 }
