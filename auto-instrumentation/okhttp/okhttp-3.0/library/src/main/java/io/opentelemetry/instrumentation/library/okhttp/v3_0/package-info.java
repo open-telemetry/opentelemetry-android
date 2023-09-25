d3914898bd8502d9f923f51a5fffab250c52b071
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2023-09-25 11:05:54 +0200

    Added package-info.java to set nonnull to all params

---
+++ auto-instrumentation/okhttp/okhttp-3.0/library/src/main/java/io/opentelemetry/instrumentation/library/okhttp/v3_0/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * OkHttp 3 auto instrumentation runtime.
+ */
+@ParametersAreNonnullByDefault
+package io.opentelemetry.instrumentation.library.okhttp.v3_0;
+
+import javax.annotation.ParametersAreNonnullByDefault;
