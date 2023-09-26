e963d34870fc2fd9f7d424bc59f9e310668ce57d
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2023-09-26 14:55:36 +0200

    Adding test for okhttp callback support

--- auto-instrumentation/okhttp/okhttp-3.0/testing/src/androidTest/java/io/opentelemetry/instrumentation/library/okhttp/v3_0/InstrumentationTest.java
+++ auto-instrumentation/okhttp/okhttp-3.0/testing/src/androidTest/java/io/opentelemetry/instrumentation/library/okhttp/v3_0/InstrumentationTest.java
@@ -8,19 +8,29 @@ package io.opentelemetry.instrumentation.library.okhttp.v3_0;
 import static org.junit.Assert.assertEquals;
 
 import androidx.annotation.NonNull;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.testing.exporter.InMemorySpanExporter;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
-import java.io.IOException;
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 
 public class InstrumentationTest {
     private MockWebServer server;
@@ -34,27 +44,62 @@ public class InstrumentationTest {
     @After
     public void tearDown() throws IOException {
         server.shutdown();
+        GlobalOpenTelemetry.resetForTest();
     }
 
     @Test
     public void okhttpTraces() throws IOException {
-        InMemorySpanExporter spanExporter = InMemorySpanExporter.create();
-        OpenTelemetrySdk.builder()
-                .setTracerProvider(getSimpleTracerProvider(spanExporter))
-                .buildAndRegisterGlobal();
+        InMemorySpanExporter spanExporter = getInMemorySpanExporter();
 
         server.enqueue(new MockResponse().setResponseCode(200));
 
         OkHttpClient client = new OkHttpClient();
-        executeRequest(client, "/test/");
+        createCall(client, "/test/").execute();
 
         assertEquals(1, spanExporter.getFinishedSpanItems().size());
     }
 
-    private void executeRequest(OkHttpClient client, String urlPath) throws IOException {
+    @Test
+    public void okhttpTraces_with_callback() throws InterruptedException {
+        CountDownLatch lock = new CountDownLatch(1);
+        InMemorySpanExporter spanExporter = getInMemorySpanExporter();
+        Span span = GlobalOpenTelemetry.getTracer("TestTracer").spanBuilder("A Span").startSpan();
+
+        try (Scope ignored = span.makeCurrent()) {
+            server.enqueue(new MockResponse().setResponseCode(200));
+
+            OkHttpClient client = new OkHttpClient();
+            createCall(client, "/test/").enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                }
+
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) {
+                    assertEquals(Context.root().with(span), Context.current());
+                    lock.countDown();
+                }
+            });
+
+        }
+
+        lock.await();
+
+        assertEquals(1, spanExporter.getFinishedSpanItems().size());
+    }
+
+    @NonNull
+    private static InMemorySpanExporter getInMemorySpanExporter() {
+        InMemorySpanExporter spanExporter = InMemorySpanExporter.create();
+        OpenTelemetrySdk.builder()
+                .setTracerProvider(getSimpleTracerProvider(spanExporter))
+                .buildAndRegisterGlobal();
+        return spanExporter;
+    }
+
+    private Call createCall(OkHttpClient client, String urlPath) {
         Request request = new Request.Builder().url(server.url(urlPath)).build();
-        Response response = client.newCall(request).execute();
-        response.body().bytes();
+        return client.newCall(request);
     }
 
     @NonNull
