1f25db0e7a209c290342672c9da074b32b69e80d
Jason Plumb <jplumb@splunk.com>
2023-09-27 15:02:22 -0700

    pin okhttp to 3.0.0

--- auto-instrumentation/okhttp/okhttp-3.0/library/build.gradle.kts
+++ auto-instrumentation/okhttp/okhttp-3.0/library/build.gradle.kts
@@ -5,7 +5,8 @@ plugins {
 
 dependencies {
     implementation(platform(project(":dependencyManagement")))
-    api("com.squareup.okhttp3:okhttp")
+    // Pin at 3.0.0 for api compatibility
+    api("com.squareup.okhttp3:okhttp:3.0.0")
     api("io.opentelemetry.instrumentation:opentelemetry-okhttp-3.0")
     implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-api-semconv")
 }
