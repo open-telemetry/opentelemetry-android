fd6b708e1a8c884f8a8b4b32108bcf403a357baf
Jason Plumb <jplumb@splunk.com>
2023-10-02 16:34:26 -0700

    Revert "This adds a dependencyManagement module to help consolidate dependencies across the project into one consistent place."
    This reverts commit c949cfa3ea4f308e6ce372eff792a2f0ef2daeda.

--- auto-instrumentation/okhttp/okhttp-3.0/testing/build.gradle.kts
+++ auto-instrumentation/okhttp/okhttp-3.0/testing/build.gradle.kts
@@ -4,10 +4,8 @@ plugins {
 }
 
 dependencies {
-    implementation(platform(project(":dependencyManagement")))
-
     byteBuddy(project(":auto-instrumentation:okhttp:okhttp-3.0:agent"))
     implementation(project(":auto-instrumentation:okhttp:okhttp-3.0:library"))
-    implementation("com.squareup.okhttp3:okhttp")
-    androidTestImplementation("com.squareup.okhttp3:mockwebserver:${rootProject.extra["okhttpVersion"]}")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    androidTestImplementation("com.squareup.okhttp3:mockwebserver:4.11.0")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,6 @@
 rootProject.name = "opentelemetry-android"
 
 include(":instrumentation")
-include(":dependencyManagement")
 include(":auto-instrumentation:okhttp:okhttp-3.0:agent")
 include(":auto-instrumentation:okhttp:okhttp-3.0:library")
 include(":auto-instrumentation:okhttp:okhttp-3.0:testing")
