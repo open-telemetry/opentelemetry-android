6d5898aaddb70238c21aa10c3ff352f7150a0c27
Jason Plumb <jplumb@splunk.com>
2023-10-02 12:47:29 -0700

    pin dependency via compileOnly (instead of api) and ignore renovate updates

--- auto-instrumentation/okhttp/okhttp-3.0/library/build.gradle.kts
+++ auto-instrumentation/okhttp/okhttp-3.0/library/build.gradle.kts
@@ -5,7 +5,8 @@ plugins {
 
 dependencies {
     // Pin at 3.0.0 for api compatibility
-    api("com.squareup.okhttp3:okhttp:3.0.0")
+    //noinspection GradleDependency
+    compileOnly("com.squareup.okhttp3:okhttp:3.0.0")
     api("io.opentelemetry.instrumentation:opentelemetry-okhttp-3.0")
     implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-api-semconv")
 }
