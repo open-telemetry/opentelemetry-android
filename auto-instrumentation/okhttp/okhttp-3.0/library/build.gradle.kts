4755df9c737b680611eaa03f72198bc651808e90
Jason Plumb <jplumb@splunk.com>
2023-10-02 16:40:19 -0700

    pin okhttp and nerf renovate

--- auto-instrumentation/okhttp/okhttp-3.0/library/build.gradle.kts
+++ auto-instrumentation/okhttp/okhttp-3.0/library/build.gradle.kts
@@ -5,7 +5,9 @@ plugins {
 
 val otelVersion = project.property("otel.sdk.version")
 dependencies {
-    api("com.squareup.okhttp3:okhttp:3.0.0")
+    // pin okhttp api at 3.0.0 for now
+    //noinspection GradleDependency
+    compileOnly("com.squareup.okhttp3:okhttp:3.0.0")
     api("io.opentelemetry.instrumentation:opentelemetry-okhttp-3.0:$otelVersion-alpha")
     implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-api-semconv:$otelVersion-alpha")
 }
