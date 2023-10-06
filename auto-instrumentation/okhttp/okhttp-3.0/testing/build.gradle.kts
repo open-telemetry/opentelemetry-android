5c83a1cba98965f98416f3082154c273a4321219
Jason Plumb <jplumb@splunk.com>
2023-10-06 11:29:15 -0700

    fix nightly build check

--- auto-instrumentation/okhttp/okhttp-3.0/testing/build.gradle.kts
+++ auto-instrumentation/okhttp/okhttp-3.0/testing/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 dependencies {
     byteBuddy(project(":auto-instrumentation:okhttp:okhttp-3.0:agent"))
-    implementation(project(":auto-instrumentation:okhttp:okhttp-3.0:library"))
+    implementation(project(":auto-instrumentation:okhttp:okhttp-3.0:agent"))
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     androidTestImplementation("com.squareup.okhttp3:mockwebserver:4.11.0")
 }
