213d35ac713e7e1e2abb8fbcd86a58b5cd462fec
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2023-12-15 11:39:52 +0100

    Created PeriodicWorkService

---
+++ instrumentation/src/test/java/io/opentelemetry/android/internal/services/periodicwork/PeriodicWorkServiceTest.kt
@@ -0,0 +1,94 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.android.internal.services.periodicwork
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Assert.fail
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.shadows.ShadowLooper
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
+@RunWith(RobolectricTestRunner::class)
+class PeriodicWorkServiceTest {
+    companion object {
+        private const val DELAY_BETWEEN_EXECUTIONS_IN_SECONDS = 15L
+    }
+
+    private lateinit var service: PeriodicWorkService
+
+    @Before
+    fun setUp() {
+        service = PeriodicWorkService()
+    }
+
+    @Test
+    fun `Execute enqueued work on start`() {
+        val latch = CountDownLatch(1)
+        var workerThreadId: Long? = null
+        service.enqueue {
+            workerThreadId = Thread.currentThread().id
+            latch.countDown()
+        }
+
+        service.start()
+        latch.await()
+
+        assertThat(workerThreadId).isNotNull()
+        assertThat(workerThreadId).isNotEqualTo(Thread.currentThread().id)
+    }
+
+    @Test
+    fun `Start only once`() {
+        val latch = CountDownLatch(1)
+
+        // First start (should work)
+        service.enqueue {
+            latch.countDown()
+        }
+        service.start()
+        latch.await()
+
+        // Trying to re-start (should not work)
+        service.enqueue {
+            fail("Must not execute this right away.")
+        }
+        service.start()
+
+        Thread.sleep(200) // Giving some time for the test to fail.
+    }
+
+    @Test
+    fun `Check for pending work after a delay`() {
+        val firstRunLatch = CountDownLatch(1)
+        val secondRunLatch = CountDownLatch(1)
+        var secondRunExecuted = false
+
+        // First run right away
+        service.enqueue {
+            firstRunLatch.countDown()
+        }
+        service.start()
+        service.enqueue {
+            secondRunExecuted = true
+            secondRunLatch.countDown()
+        }
+        firstRunLatch.await()
+        assertThat(secondRunExecuted).isFalse()
+
+        // Second run after delay
+        fastForwardBySeconds(DELAY_BETWEEN_EXECUTIONS_IN_SECONDS)
+        secondRunLatch.await(1, TimeUnit.SECONDS)
+        assertThat(secondRunExecuted).isTrue()
+    }
+
+    private fun fastForwardBySeconds(seconds: Long) {
+        ShadowLooper.idleMainLooper(seconds, TimeUnit.SECONDS)
+    }
+}
