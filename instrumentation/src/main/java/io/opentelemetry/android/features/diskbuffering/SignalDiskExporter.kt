988e2f68c1524d80036ebe567f69003445082144
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-01-12 17:19:22 +0100

    Adding javadocs

--- instrumentation/src/main/java/io/opentelemetry/android/features/diskbuffering/SignalDiskExporter.kt
+++ instrumentation/src/main/java/io/opentelemetry/android/features/diskbuffering/SignalDiskExporter.kt
@@ -21,6 +21,14 @@ class SignalDiskExporter internal constructor(
     private val logRecordDiskExporter: LogRecordDiskExporter?,
     private val exportTimeoutInMillis: Long,
 ) {
+    /**
+     * A batch contains all the signals that arrived in one call to [SpanDiskExporter.export]. So if
+     * that function is called 5 times, then there will be 5 batches in disk. This function reads
+     * and exports ONE batch every time is called.
+     *
+     * @return TRUE if it found data in disk and the exporter succeeded. FALSE if any of those conditions were
+     * not met.
+     */
     @WorkerThread
     @Throws(IOException::class)
     fun exportBatchOfSpans(): Boolean {
@@ -30,6 +38,14 @@ class SignalDiskExporter internal constructor(
         ) ?: false
     }
 
+    /**
+     * A batch contains all the signals that arrived in one call to [MetricDiskExporter.export]. So if
+     * that function is called 5 times, then there will be 5 batches in disk. This function reads
+     * and exports ONE batch every time is called.
+     *
+     * @return TRUE if it found data in disk and the exporter succeeded. FALSE if any of those conditions were
+     * not met.
+     */
     @WorkerThread
     @Throws(IOException::class)
     fun exportBatchOfMetrics(): Boolean {
@@ -39,6 +55,14 @@ class SignalDiskExporter internal constructor(
         ) ?: false
     }
 
+    /**
+     * A batch contains all the signals that arrived in one call to [LogRecordDiskExporter.export]. So if
+     * that function is called 5 times, then there will be 5 batches in disk. This function reads
+     * and exports ONE batch every time is called.
+     *
+     * @return TRUE if it found data in disk and the exporter succeeded. FALSE if any of those conditions were
+     * not met.
+     */
     @WorkerThread
     @Throws(IOException::class)
     fun exportBatchOfLogs(): Boolean {
@@ -48,6 +72,12 @@ class SignalDiskExporter internal constructor(
         ) ?: false
     }
 
+    /**
+     * Convenience method that attempts to export all kinds of signals from disk.
+     *
+     * @return TRUE if at least one of the signals were successfully exported, FALSE if no signal
+     * of any kind was exported.
+     */
     @WorkerThread
     @Throws(IOException::class)
     fun exportBatchOfEach(): Boolean {
