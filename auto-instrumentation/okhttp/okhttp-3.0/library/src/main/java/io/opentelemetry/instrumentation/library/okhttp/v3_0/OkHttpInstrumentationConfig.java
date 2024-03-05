03ab08c450a209598997b3e3ecc34d07683b5e79
Jason Plumb <jplumb@splunk.com>
2024-03-04 16:36:59 -0800

    update upstream version to 2.1

--- auto-instrumentation/okhttp/okhttp-3.0/library/src/main/java/io/opentelemetry/instrumentation/library/okhttp/v3_0/OkHttpInstrumentationConfig.java
+++ auto-instrumentation/okhttp/okhttp-3.0/library/src/main/java/io/opentelemetry/instrumentation/library/okhttp/v3_0/OkHttpInstrumentationConfig.java
@@ -5,7 +5,7 @@
 
 package io.opentelemetry.instrumentation.library.okhttp.v3_0;
 
-import io.opentelemetry.instrumentation.api.instrumenter.net.PeerServiceResolver;
+import io.opentelemetry.instrumentation.api.incubator.semconv.net.PeerServiceResolver;
 import io.opentelemetry.instrumentation.api.internal.HttpConstants;
 import java.util.ArrayList;
 import java.util.HashMap;
