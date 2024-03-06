0a5168b89b97ba888f87d5e51b709ff9c77fc469
César <56847527+LikeTheSalad@users.noreply.github.com>
2024-03-06 14:24:12 +0100

    Global attrs for logs (#266)
    * Created GlobalAttributesLogRecordAppender
    
    * Configuring global attrs for logs

---
+++ instrumentation/src/main/java/io/opentelemetry/android/internal/processors/GlobalAttributesLogRecordAppender.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.android.internal.processors
+
+import io.opentelemetry.api.common.Attributes
+import io.opentelemetry.context.Context
+import io.opentelemetry.sdk.logs.LogRecordProcessor
+import io.opentelemetry.sdk.logs.ReadWriteLogRecord
+import java.util.function.Supplier
+
+internal class GlobalAttributesLogRecordAppender(private val attributesSupplier: Supplier<Attributes>) :
+    LogRecordProcessor {
+    override fun onEmit(
+        context: Context,
+        logRecord: ReadWriteLogRecord,
+    ) {
+        logRecord.setAllAttributes(attributesSupplier.get())
+    }
+}
