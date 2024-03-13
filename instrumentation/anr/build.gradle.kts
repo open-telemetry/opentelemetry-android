ed831c740e0bbbb742a36940f018255b0e7aa229
Jason Plumb <jplumb@splunk.com>
2024-03-12 19:32:38 -0700

    move anr to own module

---
+++ instrumentation/anr/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("otel.android-library-conventions")
+    id("otel.publish-conventions")
+}
+
+description = "OpenTelemetry Android ANR instrumentation"
+
+android {
+    namespace = "io.opentelemetry.android.instrumentation.anr"
+
+    defaultConfig {
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    testOptions {
+        unitTests.isReturnDefaultValues = true
+    }
+}
+
+dependencies {
+    api(platform(libs.opentelemetry.platform))
+    api(libs.opentelemetry.api)
+    api(project(":instrumentation:common-api"))
+    implementation(libs.androidx.core)
+    implementation(libs.opentelemetry.semconv)
+    implementation(libs.opentelemetry.sdk)
+    implementation(libs.opentelemetry.instrumentation.api)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "opentelemetry-android"
 include(":common")
 include(":android-agent")
 include(":instrumentation:activity")
+include(":instrumentation:anr")
 include(":instrumentation:common-api")
 include(":instrumentation:okhttp:okhttp-3.0:agent")
 include(":instrumentation:okhttp:okhttp-3.0:library")
