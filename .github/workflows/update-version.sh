5f9fb78363ce94cbd7efe99d09f80c8a863e4b7a
Jason Plumb <jplumb@splunk.com>
2024-04-09 16:59:02 -0700

    move readme update

--- .github/workflows/update-version.sh
+++ .github/workflows/update-version.sh
@@ -3,5 +3,5 @@
 version=$1
 alpha_version=${version}-alpha
 
-sed -Ei "s/(\"io.opentelemetry.android:android-agent:).*\"/\1$alpha_version\"/" README.md
+sed -Ei "s/version=.*/version=$version/" gradle.properties
 
