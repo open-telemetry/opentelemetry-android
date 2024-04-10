a694526527500c8819ef55f414805fefbd861ec5
Jason Plumb <jplumb@splunk.com>
2024-04-10 11:58:51 -0700

    button up release notes

--- .github/scripts/generate-release-notes.sh
+++ .github/scripts/generate-release-notes.sh
@@ -3,8 +3,9 @@
 # Generates release notes, like what appears in GitHub release pages.
 
 VERSION=$1
-INST_VER=$2
-SDK_VER=$3
+PRIOR_VERSION=$2
+INST_VER=$3
+SDK_VER=$4
 
 cat > /tmp/release-notes.txt << EOF
 This release is based on OpenTelemetry Java Instrumentation $INST_VER and
@@ -24,3 +25,11 @@ sed -n "0,/^## Version $VERSION /d;/^## Version /q;p" CHANGELOG.md > /tmp/CHANGE
 perl -0pe 's/(?<!\n)\n *(?!\n)(?![-*] )(?![1-9]+\. )/ /g' /tmp/CHANGELOG_SECTION.md \
     >> /tmp/release-notes.txt
 
+cat >> /tmp/release-notes.txt << EOF
+ ### 🙇 Thank you
+
+ This release was possible thanks to the following contributors:
+
+EOF
+
+.github/scripts/generate-release-contributors.sh "v${PRIOR_VERSION}" >> /tmp/release-notes.txt
