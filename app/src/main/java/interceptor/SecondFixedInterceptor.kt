95922e6b0cd01d61d9ffc4bb3900e3b5c9ccca77
Tony Tang <tonytang@uber.com>
2024-04-11 13:57:41 -0700

    return chain.proceed(chain.request().newBuilder().addHeader("fixed_header_key_1", "1").build())

--- app/src/main/java/interceptor/SecondFixedInterceptor.kt
+++ app/src/main/java/interceptor/SecondFixedInterceptor.kt
@@ -1,20 +1,13 @@
 package interceptor
 
 import okhttp3.Interceptor
-import okhttp3.Request
-import okhttp3.Request.Builder
 import okhttp3.Response
 import java.io.IOException
 
 internal class SecondFixedInterceptor : Interceptor {
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
-        return chain.proceed(request(chain))
+        return chain.proceed(chain.request().newBuilder().addHeader("fixed_header_key_2", "22").build())
     }
 
-    private fun request(chain: Interceptor.Chain): Request {
-        val requestBuilder: Builder = chain.request().newBuilder()
-        requestBuilder.addHeader("fixed_header_key_2", "fixed_header_value_2")
-        return requestBuilder.build()
-    }
 }
