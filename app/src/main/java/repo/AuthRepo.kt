3e95e6bb8b7a8c3481610df21c5b667908f6258b
Tony Tang <tonytang@uber.com>
2024-04-11 14:30:19 -0700

    Make cold launch id as a baggage in login

--- app/src/main/java/repo/AuthRepo.kt
+++ app/src/main/java/repo/AuthRepo.kt
@@ -2,21 +2,38 @@ package repo
 
 import app.AppContext
 import app.DemoApp
-import network.UserToken
+import io.opentelemetry.api.baggage.Baggage
+import io.opentelemetry.context.Context
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
+import network.UserToken
 
 class AuthRepo(private val app: AppContext) {
+    fun auth(success: Boolean): Single<UserToken> {
+        return authInternal(if (success) 1 else 0)
+    }
 
     private fun authInternal(flag: Int): Single<UserToken> {
-        return Single.defer {
-            DemoApp.appScope(app).singleApi().logIn(flag)
+        return Single.fromCallable {
+            authWithBaggage(flag)
         }.subscribeOn(Schedulers.io())
     }
 
+    private fun authWithBaggage(flag: Int): UserToken {
+        return Context.current().with(attachedBaggage()).makeCurrent().use {
+            userToken1(flag)
+        }
+    }
 
-    fun auth(success: Boolean): Single<UserToken> {
-        return authInternal(if (success) 1 else 0)
+    private fun userToken1(flag: Int): UserToken {
+        return DemoApp.appScope(app).callableApi().logIn(flag).execute().body()!!
+    }
+
+
+    private fun attachedBaggage(): Baggage {
+        return Baggage.builder()
+                .put("cold_launch_id", "fixed_cold_launch_id")
+                .build()
     }
 
 
