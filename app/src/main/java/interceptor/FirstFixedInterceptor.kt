205c2be6dce29e0a8b0554df79899fde5b069b07
Tony Tang <tonytang@uber.com>
2024-04-11 13:56:43 -0700

    Simplify FirstFixedInterceptor

--- app/src/main/java/interceptor/FirstFixedInterceptor.kt
+++ app/src/main/java/interceptor/FirstFixedInterceptor.kt
@@ -1,26 +1,12 @@
 package interceptor
 
-import io.opentelemetry.api.baggage.Baggage
-import io.opentelemetry.api.baggage.BaggageEntry
-import io.opentelemetry.context.Context
 import okhttp3.Interceptor
-import okhttp3.Request
-import okhttp3.Request.Builder
 import okhttp3.Response
 import java.io.IOException
 
 internal class FirstFixedInterceptor : Interceptor {
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
-        val originalRequest: Request = chain.request()
-        val requestBuilder: Builder = originalRequest.newBuilder()
-        // Retrieve current context and baggage
-        val currentContext = Context.current()
-        val currentBaggage = Baggage.fromContext(currentContext)
-        // Inject baggage into request headers
-        currentBaggage.forEach { key: String, value: BaggageEntry -> requestBuilder.addHeader(key, value.value) }
-        requestBuilder.addHeader("fixed_header_key_1", "fixed_header_value_1")
-        val requestWithBaggage: Request = requestBuilder.build()
-        return chain.proceed(requestWithBaggage)
+        return chain.proceed(chain.request().newBuilder().addHeader("fixed_header_key_1", "1").build())
     }
 }
