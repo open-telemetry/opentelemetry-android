ca40d82475f5b9f6a797a1e277e86dd08d4a3ecc
Tony Tang <tonytang@uber.com>
2024-04-18 15:09:33 -0700

    Add playServicesLocationVersion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation (libs.play.services.location)
     implementation(project(":instrumentation:okhttp:okhttp-3.0:library"))
     debugImplementation (libs.chuck.library)
     releaseImplementation (libs.chuck.library.no.op)
