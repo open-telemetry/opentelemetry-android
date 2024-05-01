54150428de374cbfbea14309ecbdd268e8fb535f
Jason Plumb <jplumb@splunk.com>
2024-05-01 10:31:34 -0700

    migrate SimpleTemporaryFileProvider

--- android-agent/src/main/java/io/opentelemetry/android/internal/features/persistence/SimpleTemporaryFileProvider.kt
+++ android-agent/src/main/java/io/opentelemetry/android/internal/features/persistence/SimpleTemporaryFileProvider.kt
@@ -2,26 +2,15 @@
  * Copyright The OpenTelemetry Authors
  * SPDX-License-Identifier: Apache-2.0
  */
+package io.opentelemetry.android.internal.features.persistence
 
-package io.opentelemetry.android.internal.features.persistence;
+import io.opentelemetry.contrib.disk.buffering.internal.files.TemporaryFileProvider
+import java.io.File
+import java.util.UUID
 
-import io.opentelemetry.contrib.disk.buffering.internal.files.TemporaryFileProvider;
-import java.io.File;
-import java.util.UUID;
-
-/**
- * This class is internal and not for public use. Its APIs are unstable and can change at any time.
- */
-public final class SimpleTemporaryFileProvider implements TemporaryFileProvider {
-    private final File tempDir;
-
-    public SimpleTemporaryFileProvider(File tempDir) {
-        this.tempDir = tempDir;
-    }
-
-    /** Creates a unique file instance using the provided prefix and the current time in millis. */
-    @Override
-    public File createTemporaryFile(String prefix) {
-        return new File(tempDir, prefix + "_" + UUID.randomUUID() + ".tmp");
+internal class SimpleTemporaryFileProvider(private val tempDir: File) : TemporaryFileProvider {
+    /** Creates a unique file instance using the provided prefix and the current time in millis.  */
+    override fun createTemporaryFile(prefix: String): File {
+        return File(tempDir, prefix + "_" + UUID.randomUUID() + ".tmp")
     }
 }
--- android-agent/src/test/java/io/opentelemetry/android/internal/features/persistence/SimpleTemporaryFileProviderTest.kt
+++ android-agent/src/test/java/io/opentelemetry/android/internal/features/persistence/SimpleTemporaryFileProviderTest.kt
@@ -2,33 +2,30 @@
  * Copyright The OpenTelemetry Authors
  * SPDX-License-Identifier: Apache-2.0
  */
+package io.opentelemetry.android.internal.features.persistence
 
-package io.opentelemetry.android.internal.features.persistence;
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import java.io.File
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.io.File;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
-
-class SimpleTemporaryFileProviderTest {
-    @TempDir File tempDir;
-
+internal class SimpleTemporaryFileProviderTest {
+    @TempDir
+    lateinit var tempDir: File
     @Test
-    void createUniqueFilesBasedOnCurrentTimeAndPrefix() throws InterruptedException {
-        SimpleTemporaryFileProvider provider = new SimpleTemporaryFileProvider(tempDir);
-        File first = provider.createTemporaryFile("a");
-        File second = provider.createTemporaryFile("b");
-        Thread.sleep(1);
-        File third = provider.createTemporaryFile("a");
-
-        assertThat(first.getName()).startsWith("a").endsWith(".tmp");
-        assertThat(second.getName()).startsWith("b").endsWith(".tmp");
-        assertThat(third.getName()).startsWith("a").endsWith(".tmp");
-        assertThat(first).isNotEqualTo(third);
-
-        assertThat(first.getParentFile()).isEqualTo(tempDir);
-        assertThat(second.getParentFile()).isEqualTo(tempDir);
-        assertThat(third.getParentFile()).isEqualTo(tempDir);
+    fun createUniqueFilesBasedOnCurrentTimeAndPrefix() {
+        val provider = SimpleTemporaryFileProvider(tempDir)
+        val first = provider.createTemporaryFile("a")
+        val second = provider.createTemporaryFile("b")
+        Thread.sleep(1)
+        val third = provider.createTemporaryFile("a")
+        assertThat(first.getName()).startsWith("a").endsWith(".tmp")
+        assertThat(second.getName()).startsWith("b").endsWith(".tmp")
+        assertThat(third.getName()).startsWith("a").endsWith(".tmp")
+        assertThat(first).isNotEqualTo(third)
+        assertThat(first.getParentFile()).isEqualTo(tempDir)
+        assertThat(second.getParentFile()).isEqualTo(tempDir)
+        assertThat(third.getParentFile()).isEqualTo(tempDir)
     }
 }
