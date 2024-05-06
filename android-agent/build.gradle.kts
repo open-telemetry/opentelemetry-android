37dd79fb11c229248ce3fed1562fe0b18a000be3
Jason Plumb <jplumb@splunk.com>
2024-05-06 10:19:19 -0700

    remove dead code

--- android-agent/build.gradle.kts
+++ android-agent/build.gradle.kts
@@ -72,7 +72,6 @@ dependencies {
     implementation(libs.opentelemetry.sdk)
     implementation(libs.opentelemetry.exporter.logging)
     implementation(libs.opentelemetry.instrumentation.api)
-//    implementation(libs.opentelemetry.semconv)
     implementation(libs.opentelemetry.semconv.incubating)
     implementation(libs.opentelemetry.diskBuffering)
 
