83c3fed29efbd93c71235e89e5c02ce793358140
César <56847527+LikeTheSalad@users.noreply.github.com>
2024-05-14 17:24:28 +0200

    Making demo app independent (#355)
    * Making demo app independent
    
    * Update demo-app/build.gradle.kts
    
    * Update demo-app/build.gradle.kts
    
    ---------
    
    Co-authored-by: jason plumb <75337021+breedx-splk@users.noreply.github.com>

---
+++ demo-app/settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        google()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+        google()
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
+includeBuild("..")
