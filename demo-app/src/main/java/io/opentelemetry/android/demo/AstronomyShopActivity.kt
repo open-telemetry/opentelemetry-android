94c5d6c3b9b78b615c398b3e33bc1b7e61a7cd17
jason plumb <75337021+breedx-splk@users.noreply.github.com>
2024-06-03 09:15:33 -0700

    populate the session id on screen and set up cart (#402)

--- demo-app/src/main/java/io/opentelemetry/android/demo/AstronomyShopActivity.kt
+++ demo-app/src/main/java/io/opentelemetry/android/demo/AstronomyShopActivity.kt
@@ -33,7 +33,7 @@ class AstronomyShopActivity : AppCompatActivity() {
                 setOf(
                     R.id.navigation_home,
                     R.id.navigation_dashboard,
-                    R.id.navigation_notifications,
+                    R.id.navigation_cart,
                 ),
             )
         setupActionBarWithNavController(navController, appBarConfiguration)
--- demo-app/src/main/java/io/opentelemetry/android/demo/ui/cart/CartViewModel.kt
+++ demo-app/src/main/java/io/opentelemetry/android/demo/ui/cart/CartViewModel.kt
@@ -3,16 +3,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.android.demo.ui.notifications
+package io.opentelemetry.android.demo.ui.cart
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class CartViewModel : ViewModel() {
     private val _text =
         MutableLiveData<String>().apply {
-            value = "This is notifications Fragment"
+            value = "This is cart Fragment"
         }
     val text: LiveData<String> = _text
 }
