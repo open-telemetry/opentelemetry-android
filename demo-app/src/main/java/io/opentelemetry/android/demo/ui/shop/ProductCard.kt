1c07bf361ff85344b2aa06a8c8759033ad3a1c8c
Jason Plumb <jplumb@splunk.com>
2024-06-05 11:39:46 -0700

    create product cards

--- demo-app/src/main/java/io/opentelemetry/android/demo/ui/shop/ProductCard.kt
+++ demo-app/src/main/java/io/opentelemetry/android/demo/ui/shop/ProductCard.kt
@@ -1,23 +1,48 @@
 package io.opentelemetry.android.demo.ui.shop
 
+import android.graphics.Bitmap
 import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import io.opentelemetry.android.demo.TAG
+import io.opentelemetry.android.demo.clients.ImageLoader
 import io.opentelemetry.android.demo.gothamFont
 import io.opentelemetry.android.demo.model.Product
 
 @Composable
 fun ProductCard(product: Product) {
-    Card(modifier = Modifier.fillMaxSize()
+    val imageLoader = ImageLoader(LocalContext.current)
+    val sourceProductImage = imageLoader.load(product.picture)
+    Bitmap.createScaledBitmap(sourceProductImage, 120, 120, false)
+
+//    Bitmap.createBitmap()
+    val cardColors = CardColors(
+        containerColor = Color.White, contentColor = Color.Black,
+        disabledContentColor = Color.Black, disabledContainerColor = Color.Black
+    )
+    Card(elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+        colors = cardColors,
+        modifier = Modifier.fillMaxSize()
         .height(200.dp)
         .wrapContentHeight()
         .padding(20.dp),
@@ -25,12 +50,22 @@ fun ProductCard(product: Product) {
             Log.d(TAG, "TODO: Implement me!")
         }
     ) {
-        Row(modifier = Modifier.padding(20.dp)) {
-            Row {
-                Text(product.name, fontFamily = gothamFont)
-            }
-            Row {
-                Text("$ " + product.priceValue())
+        Row(modifier = Modifier.fillMaxSize().padding(20.dp)) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Column {
+                    Image(
+                        bitmap = sourceProductImage.asImageBitmap(),
+                        contentDescription = product.name,
+                    )
+                }
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Text(product.name + "\n\n$" + product.priceValue(), fontFamily = gothamFont,
+                        style = TextStyle.Default.copy(textAlign = TextAlign.Right),
+                        fontSize = 18.sp,
+                        modifier = Modifier.padding(start = 15.dp, top = 25.dp).fillMaxWidth()
+                    )
+
+                }
             }
         }
     }
--- demo-app/src/main/java/io/opentelemetry/android/demo/ui/shop/ProductList.kt
+++ demo-app/src/main/java/io/opentelemetry/android/demo/ui/shop/ProductList.kt
@@ -1,20 +1,28 @@
 package io.opentelemetry.android.demo.ui.shop
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import io.opentelemetry.android.demo.model.Product
 
 @Composable
 fun ProductList(products: State<List<Product>>) {
-    LazyColumn(modifier = Modifier.fillMaxWidth()) {
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
         items(products.value.size) { index ->
             Row() {
                 ProductCard(products.value[index])
             }
+        }
+        item {
+            Box(
+                modifier = Modifier.height(50.dp)
+            )
         }
     }
 }
