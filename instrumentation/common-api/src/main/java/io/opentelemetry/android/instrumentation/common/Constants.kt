11458434735d5c54d9e2b13e039dfd86ba7b7b1d
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-07-03 09:48:44 +0200

    Adding FragmentLifecycleInstrumentation

---
+++ instrumentation/common-api/src/main/java/io/opentelemetry/android/instrumentation/common/Constants.kt
@@ -0,0 +1,10 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.android.instrumentation.common
+
+object Constants {
+    const val INSTRUMENTATION_SCOPE = "io.opentelemetry.lifecycle"
+}
---
+++ instrumentation/fragment/src/main/java/io/opentelemetry/android/instrumentation/fragment/FragmentLifecycleInstrumentation.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.android.instrumentation.fragment
+
+import android.app.Application
+import android.app.Application.ActivityLifecycleCallbacks
+import android.os.Build
+import io.opentelemetry.android.OpenTelemetryRum
+import io.opentelemetry.android.instrumentation.AndroidInstrumentation
+import io.opentelemetry.android.instrumentation.common.Constants.INSTRUMENTATION_SCOPE
+import io.opentelemetry.android.instrumentation.common.ScreenNameExtractor
+import io.opentelemetry.android.internal.services.ServiceManager
+import io.opentelemetry.android.internal.services.visiblescreen.fragments.RumFragmentActivityRegisterer
+import io.opentelemetry.api.trace.Tracer
+
+class FragmentLifecycleInstrumentation : AndroidInstrumentation {
+    private var screenNameExtractor = ScreenNameExtractor.DEFAULT
+    private var tracerCustomizer: (Tracer) -> Tracer = { it }
+
+    fun setTracerCustomizer(customizer: (Tracer) -> Tracer) {
+        tracerCustomizer = customizer
+    }
+
+    fun setScreenNameExtractor(screenNameExtractor: ScreenNameExtractor) {
+        this.screenNameExtractor = screenNameExtractor
+    }
+
+    override fun install(
+        application: Application,
+        openTelemetryRum: OpenTelemetryRum,
+    ) {
+        application.registerActivityLifecycleCallbacks(buildFragmentRegisterer(openTelemetryRum))
+    }
+
+    private fun buildFragmentRegisterer(openTelemetryRum: OpenTelemetryRum): ActivityLifecycleCallbacks {
+        val visibleScreenService = ServiceManager.get().getVisibleScreenService()
+        val delegateTracer: Tracer = openTelemetryRum.openTelemetry.getTracer(INSTRUMENTATION_SCOPE)
+        val fragmentLifecycle =
+            RumFragmentLifecycleCallbacks(
+                tracerCustomizer.invoke(delegateTracer),
+                visibleScreenService::previouslyVisibleScreen,
+                screenNameExtractor,
+            )
+        return if (Build.VERSION.SDK_INT < 29) {
+            RumFragmentActivityRegisterer.createPre29(fragmentLifecycle)
+        } else {
+            RumFragmentActivityRegisterer.create(fragmentLifecycle)
+        }
+    }
+}
