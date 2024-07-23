e2bb5efa3eea8ff72d5473dfce078ba050546b54
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-07-23 10:17:51 +0200

    Making SdkInitializationEvents a service

--- instrumentation/startup/src/main/java/io/opentelemetry/android/instrumentation/startup/SdkInitializationEvents.kt
+++ instrumentation/startup/src/main/java/io/opentelemetry/android/instrumentation/startup/SdkInitializationEvents.kt
@@ -5,6 +5,7 @@
 
 package io.opentelemetry.android.instrumentation.startup
 
+import com.google.auto.service.AutoService
 import io.opentelemetry.android.common.RumConstants
 import io.opentelemetry.android.instrumentation.initialization.InitializationEvents
 import io.opentelemetry.api.common.AttributeKey
@@ -17,6 +18,7 @@ import java.time.Instant
 import java.util.function.Consumer
 import java.util.function.Supplier
 
+@AutoService(InitializationEvents::class)
 class SdkInitializationEvents(private val clock: Supplier<Instant> = Supplier { Instant.now() }) :
     InitializationEvents {
     private val events = mutableListOf<Event>()
