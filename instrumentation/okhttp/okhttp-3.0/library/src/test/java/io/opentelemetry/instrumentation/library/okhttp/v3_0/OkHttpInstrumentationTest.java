655c8b939cde5bd9a54f4dd5543ff236cea2a39c
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-08-01 11:10:41 +0200

    Validating install

--- instrumentation/okhttp/okhttp-3.0/library/src/test/java/io/opentelemetry/instrumentation/library/okhttp/v3_0/OkHttpInstrumentationTest.java
+++ instrumentation/okhttp/okhttp-3.0/library/src/test/java/io/opentelemetry/instrumentation/library/okhttp/v3_0/OkHttpInstrumentationTest.java
@@ -6,15 +6,39 @@
 package io.opentelemetry.instrumentation.library.okhttp.v3_0;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
+import io.opentelemetry.android.OpenTelemetryRum;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class OkHttpInstrumentationTest {
+class OkHttpInstrumentationTest {
+    private OkHttpInstrumentation instrumentation;
+
+    @BeforeEach
+    void setUp() {
+        instrumentation = new OkHttpInstrumentation();
+    }
+
     @Test
     void validateDefaultHttpMethods() {
-        assertThat(OkHttpInstrumentation.getKnownMethods())
+        assertThat(instrumentation.getKnownMethods())
                 .containsExactlyInAnyOrder(
                         "CONNECT", "DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT",
                         "TRACE");
+    }
+
+    @Test
+    void validateDefaultRumInstance() {
+        assertThat(instrumentation.getOpenTelemetryRum()).isEqualTo(OpenTelemetryRum.noop());
+    }
+
+    @Test
+    void validateInstall() {
+        OpenTelemetryRum rum = mock();
+
+        instrumentation.install(mock(), rum);
+
+        assertThat(instrumentation.getOpenTelemetryRum()).isEqualTo(rum);
     }
 }
