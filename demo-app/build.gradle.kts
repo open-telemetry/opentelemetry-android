4a7408f43a6f76fe52c0fdc255199a16587a117f
Magda Wojtowicz <mwojtowicz@splunk.com>
2024-08-19 20:37:20 +0200

    CVV fields displays dots insted of numbers and Proceed button is disabled untill all fields aren't blank. Added kotlin.reflect to dependencies in order to check if all fields of a data class aren't blank.

--- demo-app/build.gradle.kts
+++ demo-app/build.gradle.kts
@@ -78,6 +78,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(kotlin("reflect"))
 
     implementation(libs.opentelemetry.exporter.otlp)
 
