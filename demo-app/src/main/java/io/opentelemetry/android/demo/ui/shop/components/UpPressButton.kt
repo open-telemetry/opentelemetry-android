f63fcdb6690470498638cc17527436a71a390a0d
Magda Wojtowicz <mwojtowicz@splunk.com>
2024-08-20 17:24:42 +0200

    unused import

--- demo-app/src/main/java/io/opentelemetry/android/demo/ui/shop/components/UpPressButton.kt
+++ demo-app/src/main/java/io/opentelemetry/android/demo/ui/shop/components/UpPressButton.kt
@@ -9,7 +9,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
