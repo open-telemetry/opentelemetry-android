7aa16bac4c62e6c95a3da7c7aa286c68fac29d19
Jason Plumb <jplumb@splunk.com>
2024-08-28 11:22:59 -0700

    spotless

--- core/src/main/java/io/opentelemetry/android/SessionIdEventSender.kt
+++ core/src/main/java/io/opentelemetry/android/SessionIdEventSender.kt
@@ -2,6 +2,7 @@
  * Copyright The OpenTelemetry Authors
  * SPDX-License-Identifier: Apache-2.0
  */
+
 package io.opentelemetry.android
 
 import io.opentelemetry.android.common.RumConstants.Events.EVENT_SESSION_END
@@ -13,11 +14,14 @@ import io.opentelemetry.semconv.incubating.SessionIncubatingAttributes.SESSION_I
 import io.opentelemetry.semconv.incubating.SessionIncubatingAttributes.SESSION_PREVIOUS_ID
 
 internal class SessionIdEventSender(private val eventLogger: EventLogger) : SessionObserver {
-
-    override fun onSessionStarted(newSession: Session, previousSession: Session) {
-        val eventBuilder = eventLogger
-            .builder(EVENT_SESSION_START)
-            .put(SESSION_ID, newSession.getId())
+    override fun onSessionStarted(
+        newSession: Session,
+        previousSession: Session,
+    ) {
+        val eventBuilder =
+            eventLogger
+                .builder(EVENT_SESSION_START)
+                .put(SESSION_ID, newSession.getId())
         val previousSessionId = previousSession.getId()
         if (previousSessionId.isNotEmpty()) {
             eventBuilder.put(SESSION_PREVIOUS_ID, previousSessionId)
