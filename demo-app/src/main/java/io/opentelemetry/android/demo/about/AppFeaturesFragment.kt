463d75703156e305f527a6032f914d8e8c95931e
Magda Wojtowicz <mwojtowicz@splunk.com>
2024-09-03 17:36:59 +0200

    made unnecessarily global vars local

--- demo-app/src/main/java/io/opentelemetry/android/demo/about/AppFeaturesFragment.kt
+++ demo-app/src/main/java/io/opentelemetry/android/demo/about/AppFeaturesFragment.kt
@@ -11,17 +11,14 @@ import io.opentelemetry.android.demo.R
 
 class AppFeaturesFragment : Fragment() {
 
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var featureAdapter: FeatureAdapter
-
-    private val features = getFeatureList()
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_app_features, container, false)
-        recyclerView = view.findViewById(R.id.recyclerView)
+        lateinit var featureAdapter: FeatureAdapter
+        val features = getFeatureList()
+        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
 
         featureAdapter = FeatureAdapter(features)
         recyclerView.adapter = featureAdapter
