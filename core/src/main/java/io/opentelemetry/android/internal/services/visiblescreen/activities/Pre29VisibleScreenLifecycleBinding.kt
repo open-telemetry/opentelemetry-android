daee56e8b58749bffc7dbde677ddd222a755c528
Jason Plumb <jplumb@splunk.com>
2024-10-22 14:50:22 -0700

    move visible screen bindings to kotlin

--- core/src/main/java/io/opentelemetry/android/internal/services/visiblescreen/activities/Pre29VisibleScreenLifecycleBinding.kt
+++ core/src/main/java/io/opentelemetry/android/internal/services/visiblescreen/activities/Pre29VisibleScreenLifecycleBinding.kt
@@ -3,32 +3,22 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.android.internal.services.visiblescreen.activities;
+package io.opentelemetry.android.internal.services.visiblescreen.activities
 
-import android.app.Activity;
-import androidx.annotation.NonNull;
-import io.opentelemetry.android.internal.services.visiblescreen.VisibleScreenService;
+import android.app.Activity
+import io.opentelemetry.android.internal.services.visiblescreen.VisibleScreenService
 
 /**
  * An ActivityLifecycleCallbacks that is responsible for telling the VisibleScreenTracker when an
  * activity has been resumed and when an activity has been paused. It's just a glue class designed
  * for API level before 29.
  */
-public class Pre29VisibleScreenLifecycleBinding implements DefaultingActivityLifecycleCallbacks {
-
-    private final VisibleScreenService visibleScreenService;
-
-    public Pre29VisibleScreenLifecycleBinding(VisibleScreenService visibleScreenService) {
-        this.visibleScreenService = visibleScreenService;
+class Pre29VisibleScreenLifecycleBinding(private val visibleScreenService: VisibleScreenService) : DefaultingActivityLifecycleCallbacks {
+    override fun onActivityResumed(activity: Activity) {
+        visibleScreenService.activityResumed(activity)
     }
 
-    @Override
-    public void onActivityResumed(@NonNull Activity activity) {
-        visibleScreenService.activityResumed(activity);
-    }
-
-    @Override
-    public void onActivityPaused(@NonNull Activity activity) {
-        visibleScreenService.activityPaused(activity);
+    override fun onActivityPaused(activity: Activity) {
+        visibleScreenService.activityPaused(activity)
     }
 }
--- core/src/main/java/io/opentelemetry/android/internal/services/visiblescreen/activities/VisibleScreenLifecycleBinding.kt
+++ core/src/main/java/io/opentelemetry/android/internal/services/visiblescreen/activities/VisibleScreenLifecycleBinding.kt
@@ -3,31 +3,21 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.android.internal.services.visiblescreen.activities;
+package io.opentelemetry.android.internal.services.visiblescreen.activities
 
-import android.app.Activity;
-import androidx.annotation.NonNull;
-import io.opentelemetry.android.internal.services.visiblescreen.VisibleScreenService;
+import android.app.Activity
+import io.opentelemetry.android.internal.services.visiblescreen.VisibleScreenService
 
 /**
  * An ActivityLifecycleCallbacks that is responsible for telling the VisibleScreenTracker when an
  * activity has been resumed and when an activity has been paused. It's just a glue class.
  */
-public class VisibleScreenLifecycleBinding implements DefaultingActivityLifecycleCallbacks {
-
-    private final VisibleScreenService visibleScreenService;
-
-    public VisibleScreenLifecycleBinding(VisibleScreenService visibleScreenService) {
-        this.visibleScreenService = visibleScreenService;
+class VisibleScreenLifecycleBinding(private val visibleScreenService: VisibleScreenService) : DefaultingActivityLifecycleCallbacks {
+    override fun onActivityPostResumed(activity: Activity) {
+        visibleScreenService.activityResumed(activity)
     }
 
-    @Override
-    public void onActivityPostResumed(@NonNull Activity activity) {
-        visibleScreenService.activityResumed(activity);
-    }
-
-    @Override
-    public void onActivityPrePaused(@NonNull Activity activity) {
-        visibleScreenService.activityPaused(activity);
+    override fun onActivityPrePaused(activity: Activity) {
+        visibleScreenService.activityPaused(activity)
     }
 }
