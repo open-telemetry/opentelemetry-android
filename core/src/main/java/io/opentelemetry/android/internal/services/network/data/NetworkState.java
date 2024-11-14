320b34eee00aac2097c2ec580a7c735e1501d7a7
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2024-11-14 09:24:27 +0100

    Bumping otel instrumentation version

--- core/src/main/java/io/opentelemetry/android/internal/services/network/data/NetworkState.java
+++ core/src/main/java/io/opentelemetry/android/internal/services/network/data/NetworkState.java
@@ -5,17 +5,17 @@
 
 package io.opentelemetry.android.internal.services.network.data;
 
-import io.opentelemetry.semconv.incubating.NetworkIncubatingAttributes.NetworkConnectionTypeValues;
+import static io.opentelemetry.semconv.incubating.NetworkIncubatingAttributes.NetworkConnectionTypeIncubatingValues;
 
 /**
  * This class is internal and not for public use. Its APIs are unstable and can change at any time.
  */
 public enum NetworkState {
-    NO_NETWORK_AVAILABLE(NetworkConnectionTypeValues.UNAVAILABLE),
-    TRANSPORT_CELLULAR(NetworkConnectionTypeValues.CELL),
-    TRANSPORT_WIFI(NetworkConnectionTypeValues.WIFI),
-    TRANSPORT_WIRED(NetworkConnectionTypeValues.WIRED),
-    TRANSPORT_UNKNOWN(NetworkConnectionTypeValues.UNKNOWN),
+    NO_NETWORK_AVAILABLE(NetworkConnectionTypeIncubatingValues.UNAVAILABLE),
+    TRANSPORT_CELLULAR(NetworkConnectionTypeIncubatingValues.CELL),
+    TRANSPORT_WIFI(NetworkConnectionTypeIncubatingValues.WIFI),
+    TRANSPORT_WIRED(NetworkConnectionTypeIncubatingValues.WIRED),
+    TRANSPORT_UNKNOWN(NetworkConnectionTypeIncubatingValues.UNKNOWN),
     // this one doesn't seem to have an otel value at this point.
     TRANSPORT_VPN("vpn");
 
