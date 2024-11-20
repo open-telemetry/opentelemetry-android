47915a0f75a75aa4d053ab453ffc159932160fdc
Jason Plumb <jplumb@splunk.com>
2024-11-20 15:25:15 -0800

    move SessionIdLogRecordAppender.kt into internal package

--- core/src/main/java/io/opentelemetry/android/OpenTelemetryRumBuilder.java
+++ core/src/main/java/io/opentelemetry/android/OpenTelemetryRumBuilder.java
@@ -23,6 +23,7 @@ import io.opentelemetry.android.internal.features.persistence.DiskManager;
 import io.opentelemetry.android.internal.features.persistence.SimpleTemporaryFileProvider;
 import io.opentelemetry.android.internal.initialization.InitializationEvents;
 import io.opentelemetry.android.internal.processors.GlobalAttributesLogRecordAppender;
+import io.opentelemetry.android.internal.processors.SessionIdLogRecordAppender;
 import io.opentelemetry.android.internal.services.CacheStorage;
 import io.opentelemetry.android.internal.services.Preferences;
 import io.opentelemetry.android.internal.services.ServiceManager;
