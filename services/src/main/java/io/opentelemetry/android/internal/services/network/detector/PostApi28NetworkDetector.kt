f8be7e422106985afaba27f5767cbf2dd76ae56c
Jason Plumb <jplumb@splunk.com>
2025-01-15 17:32:19 -0800

    refactor to simplify

--- services/src/main/java/io/opentelemetry/android/internal/services/network/detector/PostApi28NetworkDetector.kt
+++ services/src/main/java/io/opentelemetry/android/internal/services/network/detector/PostApi28NetworkDetector.kt
@@ -2,6 +2,7 @@
  * Copyright The OpenTelemetry Authors
  * SPDX-License-Identifier: Apache-2.0
  */
+
 package io.opentelemetry.android.internal.services.network.detector
 
 import android.Manifest
@@ -15,44 +16,60 @@ import android.telephony.TelephonyManager
 import android.telephony.TelephonyManager.*
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
+import io.opentelemetry.android.common.internal.features.networkattributes.data.Carrier
 import io.opentelemetry.android.common.internal.features.networkattributes.data.CurrentNetwork
 import io.opentelemetry.android.common.internal.features.networkattributes.data.NetworkState
+import io.opentelemetry.android.common.internal.features.networkattributes.data.NetworkState.TRANSPORT_CELLULAR
+import io.opentelemetry.android.common.internal.features.networkattributes.data.NetworkState.TRANSPORT_VPN
+import io.opentelemetry.android.common.internal.features.networkattributes.data.NetworkState.TRANSPORT_WIFI
 import io.opentelemetry.android.internal.services.network.CarrierFinder
 import io.opentelemetry.android.internal.services.network.CurrentNetworkProvider
+import io.opentelemetry.android.internal.services.network.CurrentNetworkProvider.UNKNOWN_NETWORK
 
 /**
  * This class is internal and not for public use. Its APIs are unstable and can change at any time.
  */
 @RequiresApi(api = Build.VERSION_CODES.P)
+//TODO: Refactor tests and remove open
 internal open class PostApi28NetworkDetector(
     private val connectivityManager: ConnectivityManager,
     private val telephonyManager: TelephonyManager,
     private val carrierFinder: CarrierFinder,
     private val context: Context
 ) : NetworkDetector {
+    
     @SuppressLint("MissingPermission")
     override fun detectCurrentNetwork(): CurrentNetwork {
         val capabilities =
             connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
                 ?: return CurrentNetworkProvider.NO_NETWORK
-        var subType: String? = null
         val carrier = carrierFinder.get()
-        if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
-            // If the app has the permission, use it to get a subtype.
-            if (canReadPhoneState()) {
-                subType = getDataNetworkTypeName(telephonyManager.dataNetworkType)
-            }
-            return CurrentNetwork.builder(NetworkState.TRANSPORT_CELLULAR)
-                .carrier(carrier)
-                .subType(subType)
-                .build()
-        } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
-            return CurrentNetwork.builder(NetworkState.TRANSPORT_WIFI).carrier(carrier).build()
-        } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN)) {
-            return CurrentNetwork.builder(NetworkState.TRANSPORT_VPN).carrier(carrier).build()
+
+        fun hasTransport(capability: Int): Boolean = capabilities.hasCapability(capability)
+
+        val network = buildFromCarrier(carrier)
+        return when {
+            hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> withCellDataType(carrier)
+            hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> network(TRANSPORT_WIFI)
+            hasTransport(NetworkCapabilities.TRANSPORT_VPN) -> network(TRANSPORT_VPN)
+            else -> UNKNOWN_NETWORK
         }
-        // there is an active network, but it doesn't fall into the neat buckets above
-        return CurrentNetworkProvider.UNKNOWN_NETWORK
+    }
+
+    private fun buildFromCarrier(carrier: Carrier): (NetworkState) -> CurrentNetwork =
+        { CurrentNetwork.builder(it).carrier(carrier).build() }
+
+
+    private fun withCellDataType(
+        carrier: Carrier
+    ): CurrentNetwork {
+        val builder = CurrentNetwork.builder(TRANSPORT_CELLULAR)
+            .carrier(carrier)
+        if (canReadPhoneState()) {
+            val dataNetworkType = getDataNetworkTypeName(telephonyManager.dataNetworkType)
+            return builder.subType(dataNetworkType).build()
+        }
+        return builder.build();
     }
 
     // visible for testing
@@ -62,28 +79,30 @@ internal open class PostApi28NetworkDetector(
     }
 
     private fun getDataNetworkTypeName(dataNetworkType: Int): String {
-        when (dataNetworkType) {
-            NETWORK_TYPE_1xRTT -> return "1xRTT"
-            TelephonyManager.NETWORK_TYPE_CDMA -> return "CDMA"
-            TelephonyManager.NETWORK_TYPE_EDGE -> return "EDGE"
-            TelephonyManager.NETWORK_TYPE_EHRPD -> return "EHRPD"
-            TelephonyManager.NETWORK_TYPE_EVDO_0 -> return "EVDO_0"
-            TelephonyManager.NETWORK_TYPE_EVDO_A -> return "EVDO_A"
-            TelephonyManager.NETWORK_TYPE_EVDO_B -> return "EVDO_B"
-            TelephonyManager.NETWORK_TYPE_GPRS -> return "GPRS"
-            TelephonyManager.NETWORK_TYPE_GSM -> return "GSM"
-            TelephonyManager.NETWORK_TYPE_HSDPA -> return "HSDPA"
-            TelephonyManager.NETWORK_TYPE_HSPA -> return "HSPA"
-            TelephonyManager.NETWORK_TYPE_HSPAP -> return "HSPAP"
-            TelephonyManager.NETWORK_TYPE_HSUPA -> return "HSUPA"
-            TelephonyManager.NETWORK_TYPE_IDEN -> return "IDEN"
-            TelephonyManager.NETWORK_TYPE_IWLAN -> return "IWLAN"
-            TelephonyManager.NETWORK_TYPE_LTE -> return "LTE"
-            TelephonyManager.NETWORK_TYPE_NR -> return "NR"
-            TelephonyManager.NETWORK_TYPE_TD_SCDMA -> return "SCDMA"
-            TelephonyManager.NETWORK_TYPE_UMTS -> return "UMTS"
-            TelephonyManager.NETWORK_TYPE_UNKNOWN -> return "UNKNOWN"
+        return when (dataNetworkType) {
+            NETWORK_TYPE_1xRTT -> "1xRTT"
+            NETWORK_TYPE_CDMA ->  "CDMA"
+            NETWORK_TYPE_EDGE ->  "EDGE"
+            NETWORK_TYPE_EHRPD ->  "EHRPD"
+            NETWORK_TYPE_EVDO_0 ->  "EVDO_0"
+            NETWORK_TYPE_EVDO_A ->  "EVDO_A"
+            NETWORK_TYPE_EVDO_B ->  "EVDO_B"
+            NETWORK_TYPE_GPRS ->  "GPRS"
+            NETWORK_TYPE_GSM ->  "GSM"
+            NETWORK_TYPE_HSDPA ->  "HSDPA"
+            NETWORK_TYPE_HSPA ->  "HSPA"
+            NETWORK_TYPE_HSPAP ->  "HSPAP"
+            NETWORK_TYPE_HSUPA ->  "HSUPA"
+            NETWORK_TYPE_IDEN ->  "IDEN"
+            NETWORK_TYPE_IWLAN ->  "IWLAN"
+            NETWORK_TYPE_LTE ->  "LTE"
+            NETWORK_TYPE_NR ->  "NR"
+            NETWORK_TYPE_TD_SCDMA ->  "SCDMA"
+            NETWORK_TYPE_UMTS ->  "UMTS"
+            NETWORK_TYPE_UNKNOWN ->  "UNKNOWN"
+            else -> "UNKNOWN"
         }
-        return "UNKNOWN"
     }
+
+    private fun network(state: NetworkState): CurrentNetwork = CurrentNetwork.builder(state).build()
 }
