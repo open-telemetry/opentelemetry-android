ec7cb2bc7bc6b34979909727d6b31cb7fb6396da
Jason Plumb <jplumb@splunk.com>
2025-01-15 21:22:04 -0800

    convert to kotlin

--- services/src/main/java/io/opentelemetry/android/internal/services/network/detector/NetworkDetector.kt
+++ services/src/main/java/io/opentelemetry/android/internal/services/network/detector/NetworkDetector.kt
@@ -3,32 +3,40 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.android.internal.services.network.detector;
+package io.opentelemetry.android.internal.services.network.detector
 
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.os.Build;
-import android.telephony.TelephonyManager;
-import io.opentelemetry.android.common.internal.features.networkattributes.data.CurrentNetwork;
-import io.opentelemetry.android.internal.services.network.CarrierFinder;
+import android.content.Context
+import android.net.ConnectivityManager
+import android.os.Build
+import android.telephony.TelephonyManager
+import io.opentelemetry.android.common.internal.features.networkattributes.data.CurrentNetwork
+import io.opentelemetry.android.internal.services.network.CarrierFinder
 
 /**
  * This class is internal and not for public use. Its APIs are unstable and can change at any time.
  */
-public interface NetworkDetector {
-    CurrentNetwork detectCurrentNetwork();
+interface NetworkDetector {
+    fun detectCurrentNetwork(): CurrentNetwork
 
-    static NetworkDetector create(Context context) {
-        // TODO: Use ServiceManager to get the ConnectivityManager (not yet managed)
-        ConnectivityManager connectivityManager =
-                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            TelephonyManager telephonyManager =
-                    (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-            CarrierFinder carrierFinder = new CarrierFinder(telephonyManager);
-            return new PostApi28NetworkDetector(
-                    connectivityManager, telephonyManager, carrierFinder, context);
+    companion object {
+        @JvmStatic
+        fun create(context: Context): NetworkDetector {
+            // TODO: Use ServiceManager to get the ConnectivityManager or similar (not yet managed/abstracted)
+            val connectivityManager =
+                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                // TODO: Use ServiceManager to get the TelephonyManager or similar (not yet managed/abstracted)
+                val telephonyManager =
+                    context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+                val carrierFinder = CarrierFinder(telephonyManager)
+                return PostApi28NetworkDetector(
+                    connectivityManager,
+                    telephonyManager,
+                    carrierFinder,
+                    context,
+                )
+            }
+            return SimpleNetworkDetector(connectivityManager)
         }
-        return new SimpleNetworkDetector(connectivityManager);
     }
 }
