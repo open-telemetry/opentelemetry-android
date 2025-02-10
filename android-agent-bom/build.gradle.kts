b65073aba9374f26e8c1a90d829b49b40f647a3c
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-02-10 11:16:24 +0100

    Automatically finding projects to add to the bom

--- android-agent-bom/build.gradle.kts
+++ android-agent-bom/build.gradle.kts
@@ -5,10 +5,16 @@ plugins {
 
 dependencies {
     constraints {
-        api(project(":android-agent"))
-        api(project(":common"))
-        api(project(":core"))
-        api(project(":services"))
-        api(project(":session"))
+        rootProject.subprojects(
+            object : Action<Project> {
+                override fun execute(subproject: Project) {
+                    if (subproject != project) {
+                        subproject.plugins.withId("maven-publish") {
+                            api(subproject)
+                        }
+                    }
+                }
+            },
+        )
     }
 }
