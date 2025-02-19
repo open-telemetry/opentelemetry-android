f548c704b02c37df3c2f7503c6be97a58c0e08b8
deividasstr <deividas.strioga@gmail.com>
2025-02-19 11:34:19 +0200

    Remove maxFolderSize calculation caching

--- core/src/main/java/io/opentelemetry/android/internal/features/persistence/DiskManager.kt
+++ core/src/main/java/io/opentelemetry/android/internal/features/persistence/DiskManager.kt
@@ -9,13 +9,11 @@ import android.util.Log
 import io.opentelemetry.android.common.RumConstants
 import io.opentelemetry.android.features.diskbuffering.DiskBufferingConfig
 import io.opentelemetry.android.internal.services.CacheStorage
-import io.opentelemetry.android.internal.services.Preferences
 import java.io.File
 import java.io.IOException
 
 internal class DiskManager(
     private val cacheStorage: CacheStorage,
-    private val preferences: Preferences,
     private val diskBufferingConfig: DiskBufferingConfig,
 ) {
     @get:Throws(IOException::class)
@@ -43,19 +41,10 @@ internal class DiskManager(
          * @return The calculated size is stored in the preferences and returned.
          */
         get() {
-            val storedSize = preferences.retrieveInt(MAX_FOLDER_SIZE_KEY, -1)
-            if (storedSize > 0) {
-                Log.d(
-                    RumConstants.OTEL_RUM_LOG_TAG,
-                    String.format("Returning max folder size from preferences: %s", storedSize),
-                )
-                return storedSize
-            }
             val requestedSize = diskBufferingConfig.maxCacheSize
 
             // Divides the available cache size by 3 (for each signal's folder)
             val calculatedSize = requestedSize / 3
-            preferences.store(MAX_FOLDER_SIZE_KEY, calculatedSize)
             Log.d(
                 RumConstants.OTEL_RUM_LOG_TAG,
                 String.format(
@@ -71,7 +60,6 @@ internal class DiskManager(
         get() = diskBufferingConfig.maxCacheFileSize
 
     companion object {
-        private const val MAX_FOLDER_SIZE_KEY = "max_signal_folder_size"
 
         private fun deleteFiles(dir: File) {
             val files = dir.listFiles()
--- core/src/test/java/io/opentelemetry/android/internal/features/persistence/DiskManagerTest.kt
+++ core/src/test/java/io/opentelemetry/android/internal/features/persistence/DiskManagerTest.kt
@@ -7,16 +7,11 @@ package io.opentelemetry.android.internal.features.persistence
 
 import io.mockk.Called
 import io.mockk.MockKAnnotations
-import io.mockk.Runs
-import io.mockk.clearMocks
-import io.mockk.confirmVerified
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
-import io.mockk.just
 import io.mockk.verify
 import io.opentelemetry.android.features.diskbuffering.DiskBufferingConfig
 import io.opentelemetry.android.internal.services.CacheStorage
-import io.opentelemetry.android.internal.services.Preferences
 import io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.assertThat
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
@@ -27,9 +22,6 @@ import java.io.File
 internal class DiskManagerTest {
     @MockK
     lateinit var cacheStorage: CacheStorage
-
-    @MockK
-    lateinit var preferences: Preferences
 
     @MockK
     lateinit var diskBufferingConfig: DiskBufferingConfig
@@ -43,7 +35,7 @@ internal class DiskManagerTest {
         MockKAnnotations.init(this)
         every { cacheStorage.cacheDir }.returns(cacheDir)
         diskManager =
-            DiskManager(cacheStorage, preferences, diskBufferingConfig)
+            DiskManager(cacheStorage, diskBufferingConfig)
     }
 
     @Test
@@ -90,30 +82,11 @@ internal class DiskManagerTest {
         val maxCacheFileSize = 1024 * 1024 // 1 MB
         every { diskBufferingConfig.maxCacheSize }.returns(maxCacheSize.toInt())
         every { diskBufferingConfig.maxCacheFileSize }.returns(maxCacheFileSize)
-        every { preferences.retrieveInt(MAX_FOLDER_SIZE_KEY, -1) }.returns(-1)
-        every { preferences.store(any(), any()) } just Runs
 
         // Expects the size of a single signal type folder, to use as temporary space for reading.
         val expected = (maxCacheSize / 3).toInt()
         assertThat(diskManager.maxFolderSize).isEqualTo(expected)
-        verify {
-            preferences.store(MAX_FOLDER_SIZE_KEY, expected)
-        }
 
-        // On a second call, should get the value from the preferences.
-        clearMocks(cacheStorage, diskBufferingConfig, preferences)
-        every { preferences.retrieveInt(MAX_FOLDER_SIZE_KEY, -1) }.returns(expected)
-        assertThat(diskManager.maxFolderSize).isEqualTo(expected)
-
-        verify {
-            preferences.retrieveInt(MAX_FOLDER_SIZE_KEY, -1)
-        }
-        confirmVerified(preferences)
         verify { cacheStorage wasNot Called }
-        verify { diskBufferingConfig wasNot Called }
-    }
-
-    companion object {
-        private const val MAX_FOLDER_SIZE_KEY = "max_signal_folder_size"
     }
 }
