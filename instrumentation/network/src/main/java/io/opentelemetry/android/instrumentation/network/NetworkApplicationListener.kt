07991aca1d01f2fdaca581c3137990a0c334b73a
Jason Plumb <jplumb@splunk.com>
2025-03-20 13:02:35 -0700

    migrate network instrumentation to kotlin and simply by removing NetworkChangeMonitor.

--- instrumentation/network/src/main/java/io/opentelemetry/android/instrumentation/network/NetworkApplicationListener.kt
+++ instrumentation/network/src/main/java/io/opentelemetry/android/instrumentation/network/NetworkApplicationListener.kt
@@ -3,80 +3,67 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.android.instrumentation.network;
+package io.opentelemetry.android.instrumentation.network
 
-import static io.opentelemetry.api.common.AttributeKey.stringKey;
+import io.opentelemetry.android.common.internal.features.networkattributes.data.CurrentNetwork
+import io.opentelemetry.android.internal.services.applifecycle.ApplicationStateListener
+import io.opentelemetry.android.internal.services.network.CurrentNetworkProvider
+import io.opentelemetry.android.internal.services.network.NetworkChangeListener
+import io.opentelemetry.api.common.AttributeKey
+import io.opentelemetry.api.common.Attributes
+import io.opentelemetry.api.incubator.logs.ExtendedLogRecordBuilder
+import io.opentelemetry.api.logs.Logger
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.function.Consumer
 
-import io.opentelemetry.android.common.internal.features.networkattributes.data.CurrentNetwork;
-import io.opentelemetry.android.internal.services.applifecycle.ApplicationStateListener;
-import io.opentelemetry.android.internal.services.network.CurrentNetworkProvider;
-import io.opentelemetry.android.internal.services.network.NetworkChangeListener;
-import io.opentelemetry.api.common.AttributeKey;
-import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.api.common.AttributesBuilder;
-import io.opentelemetry.api.incubator.logs.ExtendedLogRecordBuilder;
-import io.opentelemetry.api.logs.Logger;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.function.BiConsumer;
+val NETWORK_STATUS_KEY: AttributeKey<String> = AttributeKey.stringKey("network.status")
 
-class NetworkApplicationListener implements ApplicationStateListener {
-    static final AttributeKey<String> NETWORK_STATUS_KEY = stringKey("network.status");
+internal class NetworkApplicationListener(
+    private val currentNetworkProvider: CurrentNetworkProvider,
+) : ApplicationStateListener {
+    private val shouldEmitChangeEvents = AtomicBoolean(true)
 
-    private final CurrentNetworkProvider currentNetworkProvider;
-    private final AtomicBoolean shouldEmitChangeEvents = new AtomicBoolean(true);
-
-    NetworkApplicationListener(CurrentNetworkProvider currentNetworkProvider) {
-        this.currentNetworkProvider = currentNetworkProvider;
-    }
-
-    void startMonitoring(
-            Logger eventLogger,
-            List<BiConsumer<AttributesBuilder, CurrentNetwork>> additionalExtractors) {
+    fun startMonitoring(
+        eventLogger: Logger,
+        additionalExtractors: List<NetworkAttributesExtractor>,
+    ) {
         currentNetworkProvider.addNetworkChangeListener(
-                new TracingNetworkChangeListener(
-                        eventLogger, shouldEmitChangeEvents, additionalExtractors));
+            TracingNetworkChangeListener(
+                eventLogger,
+                shouldEmitChangeEvents,
+                additionalExtractors,
+            ),
+        )
     }
 
-    @Override
-    public void onApplicationForegrounded() {
-        shouldEmitChangeEvents.set(true);
+    override fun onApplicationForegrounded() {
+        shouldEmitChangeEvents.set(true)
     }
 
-    @Override
-    public void onApplicationBackgrounded() {
-        shouldEmitChangeEvents.set(false);
+    override fun onApplicationBackgrounded() {
+        shouldEmitChangeEvents.set(false)
     }
 
-    private static final class TracingNetworkChangeListener implements NetworkChangeListener {
-
-        private final AtomicBoolean shouldEmitChangeEvents;
-        private final Logger eventLogger;
-        private final List<BiConsumer<AttributesBuilder, CurrentNetwork>> additionalExtractors;
-
-        TracingNetworkChangeListener(
-                Logger eventLogger,
-                AtomicBoolean shouldEmitChangeEvents,
-                List<BiConsumer<AttributesBuilder, CurrentNetwork>> additionalExtractors) {
-            this.eventLogger = eventLogger;
-            this.shouldEmitChangeEvents = shouldEmitChangeEvents;
-            this.additionalExtractors = additionalExtractors;
-        }
-
-        @Override
-        public void onNetworkChange(CurrentNetwork currentNetwork) {
+    private class TracingNetworkChangeListener(
+        private val eventLogger: Logger,
+        private val shouldEmitChangeEvents: AtomicBoolean,
+        private val additionalExtractors: List<NetworkAttributesExtractor>,
+    ) : NetworkChangeListener {
+        override fun onNetworkChange(currentNetwork: CurrentNetwork) {
             if (!shouldEmitChangeEvents.get()) {
-                return;
+                return
             }
-            AttributesBuilder attributesBuilder = Attributes.builder();
+            val attributesBuilder = Attributes.builder()
             additionalExtractors.forEach(
-                    extractor -> extractor.accept(attributesBuilder, currentNetwork));
-
-            ExtendedLogRecordBuilder builder =
-                    (ExtendedLogRecordBuilder) eventLogger.logRecordBuilder();
-            builder.setEventName("network.change")
-                    .setAllAttributes(attributesBuilder.build())
-                    .emit();
+                Consumer { extractor: NetworkAttributesExtractor ->
+                    extractor(attributesBuilder, currentNetwork)
+                },
+            )
+            val builder = eventLogger.logRecordBuilder() as ExtendedLogRecordBuilder
+            builder
+                .setEventName("network.change")
+                .setAllAttributes(attributesBuilder.build())
+                .emit()
         }
     }
 }
