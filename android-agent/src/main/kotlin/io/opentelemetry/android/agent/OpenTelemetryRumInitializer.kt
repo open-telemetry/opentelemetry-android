1e96878e48b4e0c058320c3c1cbd1a59f9fd1e87
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-04-15 12:14:09 +0200

    Creating OpenTelemetryRumInitializer

---
+++ android-agent/src/main/kotlin/io/opentelemetry/android/agent/OpenTelemetryRumInitializer.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.android.agent
+
+import android.app.Application
+import io.opentelemetry.android.OpenTelemetryRum
+import io.opentelemetry.android.agent.connectivity.EndpointConnectivity
+import io.opentelemetry.android.agent.connectivity.HttpEndpointConnectivity
+import io.opentelemetry.android.config.OtelRumConfig
+
+object OpenTelemetryRumInitializer {
+    @JvmStatic
+    fun initialize(
+        application: Application,
+        endpointBaseUrl: String,
+        endpointHeaders: Map<String, String> = emptyMap(),
+        spanEndpointConnectivity: EndpointConnectivity =
+            HttpEndpointConnectivity.forTraces(
+                endpointBaseUrl,
+                endpointHeaders,
+            ),
+        logEndpointConnectivity: EndpointConnectivity =
+            HttpEndpointConnectivity.forLogs(
+                endpointBaseUrl,
+                endpointHeaders,
+            ),
+        metricEndpointConnectivity: EndpointConnectivity =
+            HttpEndpointConnectivity.forMetrics(
+                endpointBaseUrl,
+                endpointHeaders,
+            ),
+        rumConfig: OtelRumConfig = OtelRumConfig(),
+    ): OpenTelemetryRum {
+        TODO()
+    }
+}
