fdda96d79ca8a1ff52bba59779a96b8fd3ff3628
GerardPaligot <gerard.paligot@gmail.com>
2025-05-07 13:57:21 +0200

    feat: add session-based sampling ratio support
    * Integrated SessionIdRatioBasedSampler in OpenTelemetryRumBuilder.
    * Enhanced SessionConfig to support sampling ratio configuration.
    
    Issue #841

--- core/src/main/java/io/opentelemetry/android/config/SessionConfig.kt
+++ core/src/main/java/io/opentelemetry/android/config/SessionConfig.kt
@@ -12,9 +12,20 @@ import kotlin.time.Duration.Companion.minutes
 data class SessionConfig(
     val backgroundInactivityTimeout: Duration = 15.minutes,
     val maxLifetime: Duration = 4.hours,
+    val ratio: Double? = null,
 ) {
+    constructor(
+        backgroundInactivityTimeout: Int = 15,
+        maxLifetime: Int = 4,
+        ratio: Double? = null,
+    ) : this(
+        backgroundInactivityTimeout = backgroundInactivityTimeout.minutes,
+        maxLifetime = maxLifetime.hours,
+        ratio = ratio,
+    )
+
     companion object {
         @JvmStatic
-        fun withDefaults(): SessionConfig = SessionConfig()
+        fun withDefaults(): SessionConfig = SessionConfig(15.minutes, 4.hours)
     }
 }
