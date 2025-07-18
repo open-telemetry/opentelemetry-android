25509ccbc3534df1f8b29ca4a92df2987747485a
Jason Plumb <jplumb@splunk.com>
2025-07-18 13:19:18 -0700

    move core deps back into constraints block

--- opentelemetry-android-bom/build.gradle.kts
+++ opentelemetry-android-bom/build.gradle.kts
@@ -11,10 +11,12 @@ dependencies {
     api(platform(libs.opentelemetry.platform.alpha))
     api(platform(libs.opentelemetry.platform))
     api(platform(libs.opentelemetry.core.bom))
-    rootProject.subprojects.forEach { subproject ->
-        if (subproject != project) {
-            subproject.plugins.withId("maven-publish") {
-                api(subproject)
+    constraints {
+        rootProject.subprojects.forEach { subproject ->
+            if (subproject != project) {
+                subproject.plugins.withId("maven-publish") {
+                    api(subproject)
+                }
             }
         }
     }
