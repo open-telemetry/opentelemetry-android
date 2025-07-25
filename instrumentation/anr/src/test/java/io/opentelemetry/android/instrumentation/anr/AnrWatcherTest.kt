5b67b12735a2ca2237f21446bad32ccec5616e4f
Tony <tonzhan2@cisco.com>
2025-07-25 15:09:55 -0700

    updated ANR data from span to log event

--- instrumentation/anr/src/test/java/io/opentelemetry/android/instrumentation/anr/AnrWatcherTest.kt
+++ instrumentation/anr/src/test/java/io/opentelemetry/android/instrumentation/anr/AnrWatcherTest.kt
@@ -10,41 +10,52 @@ import io.mockk.Called
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
-import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter
+import io.opentelemetry.api.common.Attributes
+import io.opentelemetry.api.incubator.logs.ExtendedLogRecordBuilder
+import io.opentelemetry.api.logs.Logger
 import io.opentelemetry.sdk.testing.junit5.OpenTelemetryExtension
 import org.junit.Before
 import org.junit.Test
 import org.junit.jupiter.api.extension.RegisterExtension
 
 class AnrWatcherTest {
-    @JvmField
-    @RegisterExtension
-    val testing = OpenTelemetryExtension.create()
+    companion object {
+        @JvmField
+        @RegisterExtension
+        val testing = OpenTelemetryExtension.create()
+    }
 
     private lateinit var handler: Handler
     private lateinit var mainThread: Thread
-    private lateinit var instrumenter: Instrumenter<Array<StackTraceElement>, Void>
+    private lateinit var logger: Logger
+    private lateinit var logRecordBuilder: ExtendedLogRecordBuilder
 
     @Before
     fun setup() {
         handler = mockk()
-        mainThread = mockk()
-        instrumenter = mockk()
+        mainThread = Thread.currentThread()
+        logger = mockk()
+        logRecordBuilder = mockk(relaxed = true)
+
+        every { logger.logRecordBuilder() } returns logRecordBuilder
+        every { logRecordBuilder.setEventName(any()) } returns logRecordBuilder
+        every { logRecordBuilder.setAllAttributes(any<Attributes>()) } returns logRecordBuilder
+        every { logRecordBuilder.emit() } returns Unit
     }
 
     @Test
     fun mainThreadDisappearing() {
-        val anrWatcher = AnrWatcher(handler, mainThread, instrumenter)
+        val anrWatcher = AnrWatcher(handler, mainThread, logger)
         for (i in 0..4) {
             every { handler.post(any()) } returns false
             anrWatcher.run()
         }
-        verify { instrumenter wasNot Called }
+        verify { logger wasNot Called }
     }
 
     @Test
     fun noAnr() {
-        val anrWatcher = AnrWatcher(handler, mainThread, instrumenter)
+        val anrWatcher = AnrWatcher(handler, mainThread, logger)
         for (i in 0..4) {
             every { handler.post(any()) } answers {
                 val callback = it.invocation.args[0] as Runnable
@@ -54,12 +65,12 @@ class AnrWatcherTest {
 
             anrWatcher.run()
         }
-        verify { instrumenter wasNot Called }
+        verify { logger wasNot Called }
     }
 
     @Test
     fun noAnr_temporaryPause() {
-        val anrWatcher = AnrWatcher(handler, mainThread, instrumenter)
+        val anrWatcher = AnrWatcher(handler, mainThread, logger)
         for (i in 0..4) {
             val index = i
             every { handler.post(any()) } answers {
@@ -72,33 +83,35 @@ class AnrWatcherTest {
             }
             anrWatcher.run()
         }
-        verify { instrumenter wasNot Called }
+        verify { logger wasNot Called }
     }
 
     @Test
     fun anr_detected() {
-        val stackTrace: Array<StackTraceElement> = arrayOf()
-        every { instrumenter.start(any(), any()) } returns mockk()
-        every { instrumenter.end(any(), any(), any(), any()) } returns mockk()
-        every { mainThread.stackTrace } returns stackTrace
-
-        val anrWatcher = AnrWatcher(handler, mainThread, instrumenter, 1)
+        val anrWatcher = AnrWatcher(handler, mainThread, logger, emptyList(), 1)
         every { handler.post(any()) } returns true
+
         for (i in 0..4) {
             anrWatcher.run()
         }
-        verify(exactly = 1) { instrumenter.start(any(), refEq(stackTrace)) }
-        verify(exactly = 1) { instrumenter.end(any(), refEq(stackTrace), isNull(), isNull()) }
+
+        verify(exactly = 1) { logger.logRecordBuilder() }
+        verify(exactly = 1) { logRecordBuilder.setEventName("device.anr") }
+        verify(exactly = 1) { logRecordBuilder.setAllAttributes(any<Attributes>()) }
+        verify(exactly = 1) { logRecordBuilder.emit() }
+
         for (i in 0..3) {
             anrWatcher.run()
         }
-        // Still just the 1 time
-        verify(exactly = 1) { instrumenter.start(any(), refEq(stackTrace)) }
-        verify(exactly = 1) { instrumenter.end(any(), refEq(stackTrace), isNull(), isNull()) }
+
+        verify(exactly = 1) { logger.logRecordBuilder() }
+        verify(exactly = 1) { logRecordBuilder.emit() }
 
         anrWatcher.run()
 
-        verify(exactly = 2) { instrumenter.start(any(), refEq(stackTrace)) }
-        verify(exactly = 2) { instrumenter.end(any(), refEq(stackTrace), isNull(), isNull()) }
+        verify(exactly = 2) { logger.logRecordBuilder() }
+        verify(exactly = 2) { logRecordBuilder.setEventName("device.anr") }
+        verify(exactly = 2) { logRecordBuilder.setAllAttributes(any<Attributes>()) }
+        verify(exactly = 2) { logRecordBuilder.emit() }
     }
 }
