e625825d7df21a6cb57cc5857ab64200f1063599
jason plumb <75337021+breedx-splk@users.noreply.github.com>
2025-07-25 08:27:41 -0700

    need to remove these dependency declarations, as they fail sonatype validation. (#1100)

--- opentelemetry-android-bom/build.gradle.kts
+++ opentelemetry-android-bom/build.gradle.kts
@@ -9,8 +9,7 @@ javaPlatform.allowDependencies()
 
 dependencies {
     api(platform(libs.opentelemetry.platform.alpha))
-    api(platform(libs.opentelemetry.platform))
-    api(platform(libs.opentelemetry.core.bom))
+
     constraints {
         rootProject.subprojects.forEach { subproject ->
             if (subproject != project) {
