9f763cffb47ef15c145457c3047c1087f9152981
bidetofevil <hanson.ho@embrace.io>
2025-08-05 11:48:45 -0700

    Remove READ_PHONE_STATE permission and update docs

--- services/src/main/java/io/opentelemetry/android/internal/services/network/detector/PostApi28NetworkDetector.kt
+++ services/src/main/java/io/opentelemetry/android/internal/services/network/detector/PostApi28NetworkDetector.kt
@@ -6,9 +6,8 @@
 package io.opentelemetry.android.internal.services.network.detector
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.Context
-import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
@@ -34,7 +33,7 @@ import android.telephony.TelephonyManager.NETWORK_TYPE_TD_SCDMA
 import android.telephony.TelephonyManager.NETWORK_TYPE_UMTS
 import android.telephony.TelephonyManager.NETWORK_TYPE_UNKNOWN
 import androidx.annotation.RequiresApi
-import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import io.opentelemetry.android.common.internal.features.networkattributes.data.Carrier
 import io.opentelemetry.android.common.internal.features.networkattributes.data.CurrentNetwork
 import io.opentelemetry.android.common.internal.features.networkattributes.data.NetworkState
@@ -56,9 +55,10 @@ internal class PostApi28NetworkDetector
         private val telephonyManager: TelephonyManager,
         private val carrierFinder: CarrierFinder,
         private val context: Context,
-        private val readPhoneState: () -> Boolean = { canReadPhoneState(context) },
+        private val readPhoneState: () -> Boolean = {
+            ContextCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE) == PERMISSION_GRANTED
+        },
     ) : NetworkDetector {
-        @SuppressLint("MissingPermission")
         override fun detectCurrentNetwork(): CurrentNetwork {
             val capabilities =
                 connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
@@ -79,47 +79,40 @@ internal class PostApi28NetworkDetector
         private fun buildFromCarrier(carrier: Carrier): (NetworkState) -> CurrentNetwork =
             { CurrentNetwork.builder(it).carrier(carrier).build() }
 
-        private fun withCellDataType(carrier: Carrier): CurrentNetwork {
-            val builder =
-                CurrentNetwork
-                    .builder(TRANSPORT_CELLULAR)
-                    .carrier(carrier)
-            if (readPhoneState()) {
-                val dataNetworkType = getDataNetworkTypeName(telephonyManager.dataNetworkType)
-                return builder.subType(dataNetworkType).build()
-            }
-            return builder.build()
-        }
+        private fun withCellDataType(carrier: Carrier): CurrentNetwork =
+            CurrentNetwork
+                .builder(TRANSPORT_CELLULAR)
+                .carrier(carrier)
+                .subType(getDataNetworkTypeName())
+                .build()
 
-        private fun getDataNetworkTypeName(dataNetworkType: Int): String =
-            when (dataNetworkType) {
-                NETWORK_TYPE_1xRTT -> "1xRTT"
-                NETWORK_TYPE_CDMA -> "CDMA"
-                NETWORK_TYPE_EDGE -> "EDGE"
-                NETWORK_TYPE_EHRPD -> "EHRPD"
-                NETWORK_TYPE_EVDO_0 -> "EVDO_0"
-                NETWORK_TYPE_EVDO_A -> "EVDO_A"
-                NETWORK_TYPE_EVDO_B -> "EVDO_B"
-                NETWORK_TYPE_GPRS -> "GPRS"
-                NETWORK_TYPE_GSM -> "GSM"
-                NETWORK_TYPE_HSDPA -> "HSDPA"
-                NETWORK_TYPE_HSPA -> "HSPA"
-                NETWORK_TYPE_HSPAP -> "HSPAP"
-                NETWORK_TYPE_HSUPA -> "HSUPA"
-                NETWORK_TYPE_IDEN -> "IDEN"
-                NETWORK_TYPE_IWLAN -> "IWLAN"
-                NETWORK_TYPE_LTE -> "LTE"
-                NETWORK_TYPE_NR -> "NR"
-                NETWORK_TYPE_TD_SCDMA -> "SCDMA"
-                NETWORK_TYPE_UMTS -> "UMTS"
-                NETWORK_TYPE_UNKNOWN -> "UNKNOWN"
-                else -> "UNKNOWN"
+        @Suppress("MissingPermission", "CyclomaticComplexMethod")
+        private fun getDataNetworkTypeName(): String? =
+            if (readPhoneState()) {
+                when (telephonyManager.dataNetworkType) {
+                    NETWORK_TYPE_1xRTT -> "1xRTT"
+                    NETWORK_TYPE_CDMA -> "CDMA"
+                    NETWORK_TYPE_EDGE -> "EDGE"
+                    NETWORK_TYPE_EHRPD -> "EHRPD"
+                    NETWORK_TYPE_EVDO_0 -> "EVDO_0"
+                    NETWORK_TYPE_EVDO_A -> "EVDO_A"
+                    NETWORK_TYPE_EVDO_B -> "EVDO_B"
+                    NETWORK_TYPE_GPRS -> "GPRS"
+                    NETWORK_TYPE_GSM -> "GSM"
+                    NETWORK_TYPE_HSDPA -> "HSDPA"
+                    NETWORK_TYPE_HSPA -> "HSPA"
+                    NETWORK_TYPE_HSPAP -> "HSPAP"
+                    NETWORK_TYPE_HSUPA -> "HSUPA"
+                    NETWORK_TYPE_IDEN -> "IDEN"
+                    NETWORK_TYPE_IWLAN -> "IWLAN"
+                    NETWORK_TYPE_LTE -> "LTE"
+                    NETWORK_TYPE_NR -> "NR"
+                    NETWORK_TYPE_TD_SCDMA -> "SCDMA"
+                    NETWORK_TYPE_UMTS -> "UMTS"
+                    NETWORK_TYPE_UNKNOWN -> "UNKNOWN"
+                    else -> "UNKNOWN"
+                }
+            } else {
+                null
             }
     }
-
-// visible for testing
-fun canReadPhoneState(context: Context): Boolean =
-    (
-        ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE)
-            == PackageManager.PERMISSION_GRANTED
-    )
