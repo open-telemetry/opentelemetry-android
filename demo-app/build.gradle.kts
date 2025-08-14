cd5625bfb7b28a8a112a4fde50158e599d2ea733
Jamie Lynch <fractalwrench@gmail.com>
2025-08-14 16:47:31 +0100

    build: improve build perf for demo app

--- demo-app/build.gradle.kts
+++ demo-app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
     defaultConfig {
         applicationId = "io.opentelemetry.android.demo"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
