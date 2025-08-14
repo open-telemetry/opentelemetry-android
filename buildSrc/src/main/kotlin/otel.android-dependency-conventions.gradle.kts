d7f72f7add8896ff29555ea9530085f81a186bb0
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-08-14 13:54:29 +0200

    Choosing okhttp over okhttp-jvm

--- buildSrc/src/main/kotlin/otel.android-dependency-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/otel.android-dependency-conventions.gradle.kts
@@ -2,8 +2,8 @@ configurations.all {
     resolutionStrategy {
         eachDependency {
             if (requested.group == "com.squareup.okhttp3" && requested.name == "okhttp-jvm") {
-                useTarget("com.squareup.okhttp3:okhttp-android:${requested.version}")
-                because("choosing okhttp-android over okhttp-jvm")
+                useTarget("com.squareup.okhttp3:okhttp:${requested.version}")
+                because("choosing okhttp over okhttp-jvm")
             }
         }
     }
