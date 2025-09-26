e8e213bfd341af4c366dcbfd40d596f63668201e
Jamie Lynch <fractalwrench@gmail.com>
2025-09-26 22:20:10 +0100

    add Incubating annotation to unstable APIs (#1238)

---
+++ android-agent/src/main/kotlin/io/opentelemetry/android/Incubating.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.android
+
+/**
+ * Marks an API as incubating. Incubating APIs can be subject to breaking change without warning
+ * between versions, and require an opt-in annotation to prevent compiler warnings.
+ *
+ * This annotation is intended for use on opentelemetry-android's API only and should not be used
+ * by library consumers on their own code.
+ */
+@RequiresOptIn(level = RequiresOptIn.Level.WARNING)
+@Retention(AnnotationRetention.BINARY)
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.TYPEALIAS,
+)
+annotation class Incubating
