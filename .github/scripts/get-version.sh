84f1443e58efec96c72d6ffa2ad43056663475a3
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-10-16 12:35:21 +0200

    Returning json from get-version.sh

--- .github/scripts/get-version.sh
+++ .github/scripts/get-version.sh
@@ -1,3 +1,23 @@
 #!/bin/bash -e
 
-grep ^version= gradle.properties | sed s/version=//g
+otel_android_version=$(grep ^version= gradle.properties | sed s/version=// | tr -d '\r')
+
+if [[ $otel_android_version =~ ^([0-9]+)\.([0-9]+)\.([0-9]+)(-([0-9A-Za-z.-]+))?$ ]]; then
+  otel_android_version_major="${BASH_REMATCH[1]}"
+  otel_android_version_minor="${BASH_REMATCH[2]}"
+  otel_android_version_patch="${BASH_REMATCH[3]}"
+  otel_android_version_prerelease="${BASH_REMATCH[5]}"
+else
+  echo "unexpected version: '$otel_android_version'"
+  exit 1
+fi
+
+cat <<EOF
+{
+  "version": "$otel_android_version",
+  "major": "$otel_android_version_major",
+  "minor": "$otel_android_version_minor",
+  "patch": "$otel_android_version_patch",
+  "prerelease": "$otel_android_version_prerelease"
+}
+EOF
