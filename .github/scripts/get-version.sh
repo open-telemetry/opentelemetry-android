433ee81ab586f583af8c3e3fc6b4ae6a44893cff
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-10-16 15:36:32 +0200

    Providing rc version to release.yml

--- .github/scripts/get-version.sh
+++ .github/scripts/get-version.sh
@@ -2,11 +2,11 @@
 
 otel_android_version=$(grep ^version= gradle.properties | sed s/version=// | tr -d '\r')
 
-if [[ $otel_android_version =~ ^([0-9]+)\.([0-9]+)\.([0-9]+)(-([0-9A-Za-z.-]+))?$ ]]; then
+if [[ $otel_android_version =~ ^([0-9]+)\.([0-9]+)\.([0-9]+)(-(rc\.([0-9]+)))?$ ]]; then
   otel_android_version_major="${BASH_REMATCH[1]}"
   otel_android_version_minor="${BASH_REMATCH[2]}"
   otel_android_version_patch="${BASH_REMATCH[3]}"
-  otel_android_version_prerelease="${BASH_REMATCH[5]}"
+  otel_android_version_rc="${BASH_REMATCH[6]}"
 else
   echo "unexpected version: '$otel_android_version'"
   exit 1
@@ -18,6 +18,6 @@ cat <<EOF
   "major": "$otel_android_version_major",
   "minor": "$otel_android_version_minor",
   "patch": "$otel_android_version_patch",
-  "prerelease": "$otel_android_version_prerelease"
+  "rc": "$otel_android_version_rc"
 }
 EOF
