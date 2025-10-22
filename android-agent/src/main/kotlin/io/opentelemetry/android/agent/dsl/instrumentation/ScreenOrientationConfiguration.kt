ae1d0e4db05bb6dd52acd50902519422398ae74c
riyazurrazak <r.razak@draftkings.com>
2025-10-22 17:42:45 +0530

    move ScreenOrientationConfiguration to separate file

---
+++ android-agent/src/main/kotlin/io/opentelemetry/android/agent/dsl/instrumentation/ScreenOrientationConfiguration.kt
@@ -0,0 +1,31 @@
+package io.opentelemetry.android.agent.dsl.instrumentation
+
+import io.opentelemetry.android.agent.dsl.OpenTelemetryDslMarker
+import io.opentelemetry.android.config.OtelRumConfig
+import io.opentelemetry.android.instrumentation.AndroidInstrumentationLoader
+import io.opentelemetry.android.instrumentation.common.EventAttributesExtractor
+import io.opentelemetry.android.instrumentation.screen_orientation.ScreenOrientationInstrumentation
+import io.opentelemetry.android.instrumentation.screen_orientation.model.Orientation
+
+@OpenTelemetryDslMarker
+class ScreenOrientationConfiguration internal constructor(
+    private val config: OtelRumConfig
+) : WithEventAttributes<Orientation>, CanBeEnabledAndDisabled {
+    private val instrumentation: ScreenOrientationInstrumentation by lazy {
+        AndroidInstrumentationLoader.getInstrumentation(
+            ScreenOrientationInstrumentation::class.java,
+        )
+    }
+
+    override fun addAttributesExtractor(value: EventAttributesExtractor<Orientation>) {
+        instrumentation.addAttributesExtractor(value)
+    }
+
+    override fun enabled(enabled: Boolean) {
+        if (enabled) {
+            config.allowInstrumentation(instrumentation.name)
+        } else {
+            config.suppressInstrumentation(instrumentation.name)
+        }
+    }
+}
