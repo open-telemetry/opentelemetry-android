90e089a9958ccf7306d8d4642cd8ed1708df4014
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-11-05 14:32:45 +0100

    Updating tests

--- android-agent/src/test/kotlin/io/opentelemetry/android/agent/connectivity/HttpEndpointConnectivityTest.kt
+++ android-agent/src/test/kotlin/io/opentelemetry/android/agent/connectivity/HttpEndpointConnectivityTest.kt
@@ -12,16 +12,22 @@ class HttpEndpointConnectivityTest {
     @Test
     fun `Validate exporter endpoint configuration`() {
         val headers = mapOf("Authorization" to "Basic something")
-        val tracesConnectivity = HttpEndpointConnectivity.forTraces("http://some.endpoint", headers)
-        val logsConnectivity = HttpEndpointConnectivity.forLogs("http://some.endpoint/", headers)
+        val compression = Compression.NONE
+        val tracesConnectivity =
+            HttpEndpointConnectivity.forTraces("http://some.endpoint", headers, compression)
+        val logsConnectivity =
+            HttpEndpointConnectivity.forLogs("http://some.endpoint/", headers, compression)
         val metricsConnectivity =
-            HttpEndpointConnectivity.forMetrics("http://some.endpoint", headers)
+            HttpEndpointConnectivity.forMetrics("http://some.endpoint", headers, compression)
 
         assertThat(tracesConnectivity.getUrl()).isEqualTo("http://some.endpoint/v1/traces")
         assertThat(tracesConnectivity.getHeaders()).isEqualTo(headers)
+        assertThat(tracesConnectivity.getCompression()).isEqualTo(Compression.NONE)
         assertThat(logsConnectivity.getUrl()).isEqualTo("http://some.endpoint/v1/logs")
         assertThat(logsConnectivity.getHeaders()).isEqualTo(headers)
+        assertThat(logsConnectivity.getCompression()).isEqualTo(Compression.NONE)
         assertThat(metricsConnectivity.getUrl()).isEqualTo("http://some.endpoint/v1/metrics")
         assertThat(metricsConnectivity.getHeaders()).isEqualTo(headers)
+        assertThat(metricsConnectivity.getCompression()).isEqualTo(Compression.NONE)
     }
 }
