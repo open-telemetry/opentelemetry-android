2452f72b59a3d27dfa85bab09746664bcf106ec0
Mateusz Stasielowicz <mateusz.stasielowicz@globallogic.com>
2025-11-19 13:21:18 +0100

    Apply spotless changes

--- instrumentation/compose/click/src/main/kotlin/io/opentelemetry/instrumentation/compose/click/OpentelemetryModifier.kt
+++ instrumentation/compose/click/src/main/kotlin/io/opentelemetry/instrumentation/compose/click/OpentelemetryModifier.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package io.opentelemetry.instrumentation.compose.click
 
 import androidx.compose.ui.Modifier
@@ -5,17 +10,17 @@ import androidx.compose.ui.semantics.SemanticsPropertyKey
 import androidx.compose.ui.semantics.SemanticsPropertyReceiver
 import androidx.compose.ui.semantics.semantics
 
-fun Modifier.opentelemetry(name: String): Modifier {
-    return this.semantics {
+fun Modifier.opentelemetry(name: String): Modifier =
+    this.semantics {
         this.opentelemetry = name
     }
-}
 
-internal val OpentelemetrySemanticsPropertyKey: SemanticsPropertyKey<String> = SemanticsPropertyKey(
-    name = "_opentelemetry_semantics",
-    mergePolicy = { parentValue, _ ->
-        parentValue
-    }
-)
+internal val OpentelemetrySemanticsPropertyKey: SemanticsPropertyKey<String> =
+    SemanticsPropertyKey(
+        name = "_opentelemetry_semantics",
+        mergePolicy = { parentValue, _ ->
+            parentValue
+        },
+    )
 
 private var SemanticsPropertyReceiver.opentelemetry by OpentelemetrySemanticsPropertyKey
