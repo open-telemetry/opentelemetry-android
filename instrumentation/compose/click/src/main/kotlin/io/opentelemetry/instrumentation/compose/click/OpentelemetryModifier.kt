32e5d8212e3ac72a194576bd99981529c0615e6f
MateuszS <mateusz.stawicz@gmail.com>
2025-11-19 20:04:56 +0100

    Add kdoc
    (cherry picked from commit c2aa0f168f2d01ad3eb8b9eddfb63c21116357dd)

--- instrumentation/compose/click/src/main/kotlin/io/opentelemetry/instrumentation/compose/click/OpentelemetryModifier.kt
+++ instrumentation/compose/click/src/main/kotlin/io/opentelemetry/instrumentation/compose/click/OpentelemetryModifier.kt
@@ -10,6 +10,13 @@ import androidx.compose.ui.semantics.SemanticsPropertyKey
 import androidx.compose.ui.semantics.SemanticsPropertyReceiver
 import androidx.compose.ui.semantics.semantics
 
+/**
+ * An opentelemetry Modifier that allows to mark a composable element as traceable.
+ * When a composable element that has this Modifier will be tapped, then the [name] from this
+ * Modifier will be taken as the element name
+ *
+ * @param name name of the tapped element
+ */
 fun Modifier.opentelemetry(name: String): Modifier =
     this.semantics {
         this.opentelemetry = name
