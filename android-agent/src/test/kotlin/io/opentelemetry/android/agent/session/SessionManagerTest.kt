da961df351ae958269386458c3cd2c1495d09bd7
Gregory-Rasmussen_cvsh <gregory.rasmussen@cvshealth.com>
2025-11-21 20:20:33 -0800

    removed an unused import statement

--- android-agent/src/test/kotlin/io/opentelemetry/android/agent/session/SessionManagerTest.kt
+++ android-agent/src/test/kotlin/io/opentelemetry/android/agent/session/SessionManagerTest.kt
@@ -22,7 +22,6 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
 import java.util.concurrent.CountDownLatch
-import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicInteger
