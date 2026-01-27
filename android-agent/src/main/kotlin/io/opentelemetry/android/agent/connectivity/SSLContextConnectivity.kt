d6bdd6eafebfc06bee778bb76f0e0284ca913a61
damienlefebvre <damien.lefebvre@famoco.com>
2026-01-27 13:09:53 +0100

    Expose ssl context for dsl config, fix data class exposure

--- android-agent/src/main/kotlin/io/opentelemetry/android/agent/connectivity/SSLContextConnectivity.kt
+++ android-agent/src/main/kotlin/io/opentelemetry/android/agent/connectivity/SSLContextConnectivity.kt
@@ -8,7 +8,7 @@ package io.opentelemetry.android.agent.connectivity
 import javax.net.ssl.SSLContext
 import javax.net.ssl.X509TrustManager
 
-data class SSLContextConnectivity(
+class SSLContextConnectivity(
     val sslContext: SSLContext,
     val sslX509TrustManager: X509TrustManager,
 )
