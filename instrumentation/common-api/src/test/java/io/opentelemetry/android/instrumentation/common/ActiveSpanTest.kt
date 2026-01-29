580659a76ff8ff990bef824b8b736e24c349c0c5
Jamie Lynch <fractalwrench@gmail.com>
2026-01-29 14:20:36 +0000

    refactor: convert ActiveSpan to Kotlin

---
+++ instrumentation/common-api/src/test/java/io/opentelemetry/android/instrumentation/common/ActiveSpanTest.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.android.instrumentation.common
+
+import io.mockk.mockk
+import io.mockk.verify
+import io.opentelemetry.api.trace.Span
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+internal class ActiveSpanTest {
+    @Test
+    fun testActiveSpan() {
+        val activeSpan = ActiveSpan { null }
+        assertFalse(activeSpan.spanInProgress())
+
+        activeSpan.endActiveSpan()
+        activeSpan.addEvent("test")
+        activeSpan.addPreviousScreenAttribute("screenName")
+        assertFalse(activeSpan.spanInProgress())
+    }
+
+    @Test
+    fun testActiveSpanCreate() {
+        val activeSpan = ActiveSpan { "prev_screen" }
+        assertFalse(activeSpan.spanInProgress())
+
+        val span = mockk<Span>(relaxed = true)
+        activeSpan.startSpan { span }
+        assertTrue(activeSpan.spanInProgress())
+
+        val eventName = "event"
+        activeSpan.addEvent(eventName)
+        verify(exactly = 1) { span.addEvent(eventName) }
+
+        activeSpan.endActiveSpan()
+        assertFalse(activeSpan.spanInProgress())
+    }
+}
