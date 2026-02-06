c5c17f76c52a1f2341a6b648154ad309d62afa94
Jason Plumb <jplumb@splunk.com>
2026-02-06 15:22:37 -0800

    spelling

--- core/src/main/java/io/opentelemetry/android/OtelAndroidClock.kt
+++ core/src/main/java/io/opentelemetry/android/OtelAndroidClock.kt
@@ -14,7 +14,7 @@ import io.opentelemetry.sdk.common.Clock
  * adds that baseline to [SystemClock.elapsedRealtimeNanos] to provide a monotonic wall-clock time.
  *
  * This avoids the downside of [System.currentTimeMillis] not being monotonic on Android (i.e.
- * the clock doesn't consistently tick when the process is in the backround or cached). It also
+ * the clock doesn't consistently tick when the process is in the background or cached). It also
  * avoids the problem of [SystemClock.elapsedRealtimeNanos] not providing wall-clock time.
  */
 class OtelAndroidClock : Clock {
