b5258e9b5a75d4253d94c968f1f6e27844f48066
Jamie Lynch <fractalwrench@gmail.com>
2026-02-12 06:04:54 +0000

    test: alter active span assertion

--- instrumentation/common-api/src/test/java/io/opentelemetry/android/instrumentation/common/ActiveSpanTest.kt
+++ instrumentation/common-api/src/test/java/io/opentelemetry/android/instrumentation/common/ActiveSpanTest.kt
@@ -5,9 +5,13 @@
 
 package io.opentelemetry.android.instrumentation.common
 
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import io.opentelemetry.api.trace.Span
+import io.opentelemetry.api.trace.SpanContext
+import io.opentelemetry.api.trace.TraceFlags
+import io.opentelemetry.api.trace.TraceState
 import io.opentelemetry.context.Context
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
@@ -31,11 +35,26 @@ internal class ActiveSpanTest {
     fun testActiveSpanCreate() {
         val activeSpan = ActiveSpan { "prev_screen" }
         assertFalse(activeSpan.spanInProgress())
+        assertEquals(Context.root(), Context.current())
 
-        val span = mockk<Span>(relaxed = true)
+        val span =
+            mockk<Span>(relaxed = true) {
+                every { makeCurrent() } answers {
+                    val span =
+                        Span.wrap(
+                            SpanContext.create(
+                                "12345678901234561234567890123456",
+                                "1234567890123456",
+                                TraceFlags.getDefault(),
+                                TraceState.getDefault(),
+                            ),
+                        )
+                    span.makeCurrent()
+                }
+            }
         activeSpan.startSpan { span }
         assertTrue(activeSpan.spanInProgress())
-        verify(exactly = 1) { span.makeCurrent() }
+        assertNotEquals(Context.root(), Context.current())
 
         val eventName = "event"
         activeSpan.addEvent(eventName)
@@ -43,6 +62,6 @@ internal class ActiveSpanTest {
 
         activeSpan.endActiveSpan()
         assertFalse(activeSpan.spanInProgress())
-        verify(exactly = 1) { span.makeCurrent() }
+        assertEquals(Context.root(), Context.current())
     }
 }
