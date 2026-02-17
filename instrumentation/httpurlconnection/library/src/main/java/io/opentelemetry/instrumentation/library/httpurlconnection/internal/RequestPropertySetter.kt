1d4ed3394d60a74030ab3f5fc654de3ad3e37322
Jamie Lynch <fractalwrench@gmail.com>
2026-02-17 12:48:14 +0000

    refactor: convert HttpUrlConnection to kotlin

---
+++ instrumentation/httpurlconnection/library/src/main/java/io/opentelemetry/instrumentation/library/httpurlconnection/internal/RequestPropertySetter.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.library.httpurlconnection.internal
+
+import io.opentelemetry.context.propagation.TextMapSetter
+import java.net.URLConnection
+
+internal object RequestPropertySetter : TextMapSetter<URLConnection> {
+    override fun set(
+        carrier: URLConnection?,
+        key: String,
+        value: String,
+    ) {
+        carrier?.setRequestProperty(key, value)
+    }
+}
