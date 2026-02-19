ac03d68078dc384ece6a500e3e2b2b59fc87d0a2
Jamie Lynch <fractalwrench@gmail.com>
2026-02-19 00:29:17 +0000

    refactor: convert HttpUrlConnection instrumentation to Kotlin (#1558)

---
+++ instrumentation/httpurlconnection/library/src/main/java/io/opentelemetry/instrumentation/library/httpurlconnection/internal/HttpUrlHttpAttributesGetter.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.library.httpurlconnection.internal
+
+import io.opentelemetry.instrumentation.api.semconv.http.HttpClientAttributesGetter
+import java.net.HttpURLConnection
+import java.net.URLConnection
+
+internal class HttpUrlHttpAttributesGetter : HttpClientAttributesGetter<URLConnection, Int> {
+    override fun getHttpRequestMethod(connection: URLConnection): String {
+        val httpURLConnection = connection as HttpURLConnection
+        return httpURLConnection.requestMethod
+    }
+
+    override fun getUrlFull(connection: URLConnection): String = connection.getURL().toExternalForm()
+
+    override fun getHttpRequestHeader(
+        connection: URLConnection,
+        name: String,
+    ): MutableList<String?> =
+        when (val value = connection.getRequestProperty(name)) {
+            null -> mutableListOf()
+            else -> mutableListOf(value)
+        }
+
+    override fun getHttpResponseStatusCode(
+        connection: URLConnection,
+        statusCode: Int,
+        error: Throwable?,
+    ): Int = statusCode
+
+    override fun getHttpResponseHeader(
+        connection: URLConnection,
+        statusCode: Int,
+        name: String,
+    ): MutableList<String?> =
+        when (val value = connection.getHeaderField(name)) {
+            null -> mutableListOf()
+            else -> mutableListOf(value)
+        }
+
+    override fun getNetworkProtocolName(
+        connection: URLConnection,
+        integer: Int?,
+    ): String {
+        // HttpURLConnection hardcodes the protocol name&version
+        return "http"
+    }
+
+    override fun getNetworkProtocolVersion(
+        connection: URLConnection,
+        integer: Int?,
+    ): String {
+        // HttpURLConnection hardcodes the protocol name&version
+        return "1.1"
+    }
+
+    override fun getServerAddress(connection: URLConnection): String = connection.getURL().host
+
+    override fun getServerPort(connection: URLConnection): Int = connection.getURL().port
+}
