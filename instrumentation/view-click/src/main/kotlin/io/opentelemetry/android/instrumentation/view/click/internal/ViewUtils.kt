a4c03a52dc18935e13c40884f7c7358b0efe8df6
DavidGrath <49921604+DavidGrath@users.noreply.github.com>
2026-04-28 23:39:21 +0100

    Added basic double-tap, based off view click by cleverchuk. Addresses #1642 and #1576 (#1681)
    * Added basic double-tap, based off view click
    
    * Made fixes to correct failures to checks
    
    * Ran apiDump
    
    * New hardware attributes: pointer, clicks, and button. Single tap and double tap both now rely on `GestureDetector`
    
    * Make detekt and apiDump happy
    
    * Removed primary button exclusivity from double-tap
    
    * Apply suggestions from code review
    
    Co-authored-by: Jamie Lynch <fractalwrench@gmail.com>
    
    * Tap Event logic moved to a class for deduplication. Hardware attributes now also present on view click. Other reviewed changes
    
    * Updated tests to reflect recent instrumentation stabilization changes
    
    ---------
    
    Co-authored-by: Jamie Lynch <fractalwrench@gmail.com>

--- instrumentation/view-click/src/main/kotlin/io/opentelemetry/android/instrumentation/view/click/internal/ViewUtils.kt
+++ instrumentation/view-click/src/main/kotlin/io/opentelemetry/android/instrumentation/view/click/internal/ViewUtils.kt
@@ -5,5 +5,56 @@
 
 package io.opentelemetry.android.instrumentation.view.click.internal
 
+import android.view.MotionEvent
+
 internal const val APP_SCREEN_CLICK_EVENT_NAME = "app.screen.click"
 internal const val VIEW_CLICK_EVENT_NAME = "app.widget.click"
+
+internal const val HARDWARE_POINTER_TYPE = "hw.pointer.type"
+
+internal const val HARDWARE_POINTER_BUTTON = "hw.pointer.button"
+
+internal const val HARDWARE_POINTER_CLICKS = "hw.pointer.clicks"
+
+internal fun buttonStateToString(buttonStateInt: Int): String? {
+    return when(buttonStateInt) {
+        MotionEvent.BUTTON_PRIMARY, MotionEvent.BUTTON_STYLUS_PRIMARY -> "primary"
+        MotionEvent.BUTTON_SECONDARY, MotionEvent.BUTTON_STYLUS_SECONDARY -> "secondary"
+        MotionEvent.BUTTON_TERTIARY -> "tertiary"
+        MotionEvent.BUTTON_BACK -> "back"
+        MotionEvent.BUTTON_FORWARD -> "forward"
+        else -> null
+    }
+}
+
+internal fun toolTypeToString(toolTypeInt: Int): String {
+    return when(toolTypeInt) {
+        MotionEvent.TOOL_TYPE_MOUSE -> "mouse"
+        MotionEvent.TOOL_TYPE_FINGER -> "finger"
+        MotionEvent.TOOL_TYPE_STYLUS -> "stylus"
+        MotionEvent.TOOL_TYPE_ERASER -> "eraser"
+        else -> "unknown"
+    }
+}
+
+internal class TapEvent(
+    private val motionEvent: MotionEvent
+) {
+
+    val toolTypeDescription: String
+    val buttonStateDescription: String?
+
+    init {
+        val toolTypeInt = motionEvent.getToolType(0)
+        val toolTypeHasButtons =
+            toolTypeInt == MotionEvent.TOOL_TYPE_MOUSE || toolTypeInt == MotionEvent.TOOL_TYPE_STYLUS
+        val buttonStateInt = motionEvent.buttonState
+        if (toolTypeHasButtons) {
+            buttonStateDescription = buttonStateToString(buttonStateInt)
+        } else {
+            buttonStateDescription = null
+        }
+       toolTypeDescription = toolTypeToString(toolTypeInt)
+    }
+
+}
