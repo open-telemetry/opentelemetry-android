d2ea780ecd26192953691bfbfec7fcc9c8a0c1cf
Emmanuela Opurum <emmanuela_prince@yahoo.com>
2026-05-12 23:14:38 +0100

    feat: support fullUrl in EndpointConfiguration to bypass default signal path appending
    Co-authored-by: breedx-splk <breedx-splk@users.noreply.github.com>

--- android-agent/src/main/kotlin/io/opentelemetry/android/agent/connectivity/HttpEndpointConnectivity.kt
+++ android-agent/src/main/kotlin/io/opentelemetry/android/agent/connectivity/HttpEndpointConnectivity.kt
@@ -2,7 +2,6 @@
  * Copyright The OpenTelemetry Authors
  * SPDX-License-Identifier: Apache-2.0
  */
-
 package io.opentelemetry.android.agent.connectivity
 
 internal class HttpEndpointConnectivity private constructor(
@@ -10,59 +9,61 @@ internal class HttpEndpointConnectivity private constructor(
     private val headers: Map<String, String>,
     private val compression: Compression,
     private val sslContext: SSLContextConnectivity?,
-    private val clientTls: ClientTlsConnectivity? = null
+    private val clientTls: ClientTlsConnectivity? = null,
 ) : EndpointConnectivity {
     companion object {
         fun forTraces(
             baseUrl: String,
+            fullUrl: Boolean = false,
             headers: Map<String, String>,
             compression: Compression,
             sslContext: SSLContextConnectivity?,
-            clientTls: ClientTlsConnectivity?
-        ): HttpEndpointConnectivity = HttpEndpointConnectivity(
-            baseUrl.trimEnd('/') + "/v1/traces",
-            headers,
-            compression,
-            sslContext,
-            clientTls
-        )
+            clientTls: ClientTlsConnectivity?,
+        ): HttpEndpointConnectivity =
+            HttpEndpointConnectivity(
+                if (fullUrl) baseUrl else baseUrl.trimEnd('/') + "/v1/traces",
+                headers,
+                compression,
+                sslContext,
+                clientTls,
+            )
 
         fun forLogs(
             baseUrl: String,
+            fullUrl: Boolean = false,
             headers: Map<String, String>,
             compression: Compression,
             sslContext: SSLContextConnectivity?,
-            clientTls: ClientTlsConnectivity?
-        ): HttpEndpointConnectivity = HttpEndpointConnectivity(
-            baseUrl.trimEnd('/') + "/v1/logs",
-            headers,
-            compression,
-            sslContext,
-            clientTls
-        )
+            clientTls: ClientTlsConnectivity?,
+        ): HttpEndpointConnectivity =
+            HttpEndpointConnectivity(
+                if (fullUrl) baseUrl else baseUrl.trimEnd('/') + "/v1/logs",
+                headers,
+                compression,
+                sslContext,
+                clientTls,
+            )
 
         fun forMetrics(
             baseUrl: String,
+            fullUrl: Boolean = false,
             headers: Map<String, String>,
             compression: Compression,
             sslContext: SSLContextConnectivity?,
-            clientTls: ClientTlsConnectivity?
-        ): HttpEndpointConnectivity = HttpEndpointConnectivity(
-            baseUrl.trimEnd('/') + "/v1/metrics",
-            headers,
-            compression,
-            sslContext,
-            clientTls
-        )
+            clientTls: ClientTlsConnectivity?,
+        ): HttpEndpointConnectivity =
+            HttpEndpointConnectivity(
+                if (fullUrl) baseUrl else baseUrl.trimEnd('/') + "/v1/metrics",
+                headers,
+                compression,
+                sslContext,
+                clientTls,
+            )
     }
 
     override fun getUrl(): String = url
-
     override fun getHeaders(): Map<String, String> = headers
-
     override fun getCompression(): Compression = compression
-
     override fun getSslContext(): SSLContextConnectivity? = sslContext
-
     override fun getClientTls(): ClientTlsConnectivity? = clientTls
 }
