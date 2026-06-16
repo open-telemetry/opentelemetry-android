5b74bb242856859c1fb8e1055f725cfca0a8ffeb
Jason Plumb <jplumb@splunk.com>
2026-06-16 15:05:30 -0700

    add test

---
+++ common/src/test/java/io/opentelemetry/android/common/internal/SemconvCompatTest.kt
@@ -0,0 +1,37 @@
+package io.opentelemetry.android.common.internal
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+
+class SemconvCompatTest {
+
+    var compatState: Boolean = true
+
+    @AfterEach
+    fun setup() {
+        compatState = SemconvCompat.useLatestExperimental
+    }
+
+    @AfterEach
+    fun restore() {
+        SemconvCompat.useLatestExperimental = compatState
+    }
+
+    @Test
+    fun `test compat with legacy`() {
+        SemconvCompat.useLatestExperimental = false
+        assertThat(SemconvCompat.map("app.screen.name")).isEqualTo("screen.name")
+        assertThat(SemconvCompat.map("app.crash")).isEqualTo("device.crash")
+        assertThat(SemconvCompat.map("rando.semconv.thinger")).isEqualTo("rando.semconv.thinger")
+    }
+
+    @Test
+    fun `test compat with latest experimental`() {
+        SemconvCompat.useLatestExperimental = true
+        assertThat(SemconvCompat.map("app.screen.name")).isEqualTo("app.screen.name")
+        assertThat(SemconvCompat.map("app.crash")).isEqualTo("app.crash")
+        assertThat(SemconvCompat.map("rando.semconv.thinger")).isEqualTo("rando.semconv.thinger")
+    }
+
+}
