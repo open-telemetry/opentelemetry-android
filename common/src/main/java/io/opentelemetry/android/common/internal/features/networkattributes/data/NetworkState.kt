fdf2fca71b8647bc57b084334813ce15e2778d15
Jason Plumb <jplumb@splunk.com>
2026-07-02 15:02:35 -0700

    Lint remaining Kotlin sources

--- common/src/main/java/io/opentelemetry/android/common/internal/features/networkattributes/data/NetworkState.kt
+++ common/src/main/java/io/opentelemetry/android/common/internal/features/networkattributes/data/NetworkState.kt
@@ -6,11 +6,11 @@
 package io.opentelemetry.android.common.internal.features.networkattributes.data
 
 import io.opentelemetry.kotlin.semconv.IncubatingApi
-import io.opentelemetry.kotlin.semconv.NetworkAttributes.NetworkConnectionTypeValues.UNAVAILABLE
 import io.opentelemetry.kotlin.semconv.NetworkAttributes.NetworkConnectionTypeValues.CELL
+import io.opentelemetry.kotlin.semconv.NetworkAttributes.NetworkConnectionTypeValues.UNAVAILABLE
+import io.opentelemetry.kotlin.semconv.NetworkAttributes.NetworkConnectionTypeValues.UNKNOWN
 import io.opentelemetry.kotlin.semconv.NetworkAttributes.NetworkConnectionTypeValues.WIFI
 import io.opentelemetry.kotlin.semconv.NetworkAttributes.NetworkConnectionTypeValues.WIRED
-import io.opentelemetry.kotlin.semconv.NetworkAttributes.NetworkConnectionTypeValues.UNKNOWN
 
 /**
  * This class is internal and not for public use. Its APIs are unstable and can change at any time.
@@ -20,12 +20,16 @@ enum class NetworkState(
 ) {
     @OptIn(IncubatingApi::class)
     NO_NETWORK_AVAILABLE(UNAVAILABLE.value),
+
     @OptIn(IncubatingApi::class)
     TRANSPORT_CELLULAR(CELL.value),
+
     @OptIn(IncubatingApi::class)
     TRANSPORT_WIFI(WIFI.value),
+
     @OptIn(IncubatingApi::class)
     TRANSPORT_WIRED(WIRED.value),
+
     @OptIn(IncubatingApi::class)
     TRANSPORT_UNKNOWN(UNKNOWN.value),
 
