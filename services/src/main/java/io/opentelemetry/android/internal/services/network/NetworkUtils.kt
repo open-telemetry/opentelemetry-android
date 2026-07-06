dc5a996d44e8565e8ce614d1dd29befa6742e0ad
jason plumb <75337021+breedx-splk@users.noreply.github.com>
2026-07-06 08:02:53 -0700

    Remove dependency from core to network instrumentation (#1847)
    * remove dependency from core to network instrumentation
    
    * add missing headers
    # Conflicts:
    #	core/src/main/java/io/opentelemetry/android/internal/processors/NetworkAttributesSpanAppender.kt
    #	core/src/test/java/io/opentelemetry/android/internal/processors/NetworkAttributesSpanAppenderTest.kt

--- services/src/main/java/io/opentelemetry/android/internal/services/network/NetworkUtils.kt
+++ services/src/main/java/io/opentelemetry/android/internal/services/network/NetworkUtils.kt
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package io.opentelemetry.android.instrumentation.network.internal
+package io.opentelemetry.android.internal.services.network
 
 import android.Manifest
 import android.content.Context
