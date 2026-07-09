6e38cada2472a144459db98a8ad90d57d906c985
Jason Plumb <jplumb@splunk.com>
2026-07-08 17:28:22 -0700

    revert breaking api change in common, add deprecation.

--- common/src/main/java/io/opentelemetry/android/common/RumConstants.kt
+++ common/src/main/java/io/opentelemetry/android/common/RumConstants.kt
@@ -5,6 +5,13 @@
 
 package io.opentelemetry.android.common
 
+import io.opentelemetry.android.semconv.LastAttributes
+import io.opentelemetry.api.common.AttributeKey
+
 object RumConstants {
     const val OTEL_RUM_LOG_TAG: String = "OpenTelemetryRum"
+
+    @Deprecated("Use LastAttributes.LAST_SCREEN_NAME_KEY instead.")
+    @JvmField
+    val LAST_SCREEN_NAME_KEY: AttributeKey<String> = LastAttributes.LAST_SCREEN_NAME_KEY
 }
